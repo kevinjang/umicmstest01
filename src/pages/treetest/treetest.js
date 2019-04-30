963f75572cdf4d27a80e74ff5911f8c7d4144b6c
kevinjang <kevinjang@163.com>
2019-04-30 16:36:41 +0800

    Update treetest.js

--- src/pages/treetest/treetest.js
+++ src/pages/treetest/treetest.js
@@ -231,30 +231,30 @@ class EditableCell extends React.Component {
                             return (
                                 editing ? (
                                     <FormItem style={{ margin: 0 }}>
-                                        {(dataIndex !== 'InvoiceNo')?
+                                        {(dataIndex !== 'InvoiceNo') ?
                                             form.getFieldDecorator(dataIndex, {
-                                            rules: [{
-                                                required: true,
-                                                message: `${title} 是必填项.`,
-                                            }],
-                                            initialValue: (dataIndex === 'ExpenseTime' ? moment(new Date(), dateFormat) : record[dataIndex] || 0),
-                                        })(this.setControl(dataIndex, this))
-                                        :form.getFieldDecorator(dataIndex, {
-                                            rules: [{
-                                                required: true,
-                                                message: `${title} 是必填项.`,
-                                            },{                                                
-                                                max: 10,
-                                                message: '长度不符合标准'
-                                            }],
-                                            initialValue: (dataIndex === 'ExpenseTime' ? moment(new Date(), dateFormat) : record[dataIndex] || 0),
-                                        })(this.setControl(dataIndex, this))
-                                    }
+                                                rules: [{
+                                                    required: true,
+                                                    message: `${title} 是必填项.`,
+                                                }],
+                                                initialValue: (dataIndex === 'ExpenseTime' ? moment(new Date(), dateFormat) : record[dataIndex] || 0),
+                                            })(this.setControl(dataIndex, this))
+                                            : form.getFieldDecorator(dataIndex, {
+                                                rules: [{
+                                                    required: true,
+                                                    message: `${title} 是必填项.`,
+                                                }, {
+                                                    max: 10,
+                                                    message: '长度不符合标准'
+                                                }],
+                                                initialValue: (dataIndex === 'ExpenseTime' ? moment(new Date(), dateFormat) : record[dataIndex] || 0),
+                                            })(this.setControl(dataIndex, this))
+                                        }
                                     </FormItem>
                                 ) : (
                                         <div
                                             className="editable-cell-value-wrap"
-                                            style={{ paddingRight: 24, height: 30 }}
+                                            style={{ paddingRight: (dataIndex === 'InvoiceNo' ? 0 : 24), height: 30 }}
                                             onClick={this.toggleEdit}>
                                             {restProps.children}
                                         </div>
