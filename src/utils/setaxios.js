f76cf4671bde1cfa0c2a81eac17da6ee3388b9d7
kevinjang <kevinjang@163.com>
2019-09-02 17:34:35 +0800

    Update setaxios.js
    折腾疯了。

--- src/utils/setaxios.js
+++ src/utils/setaxios.js
@@ -1,28 +1,30 @@
 import axios from 'axios'
 const setAxios = () => {
 
-    axios.defaults.baseURL = "http://localhost:3000/";
+    axios.defaults.baseURL = "http://localhost:3000";
 
     axios.interceptors.request.use(request => {
         // console.log('request.headers',request.headers)
-        request.headers["Access-Control-Allow-Origin"] = "*";
-        
+        request.headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
+        // request.setHeader
+        console.log('axios-request-set', request.headers);
         // // request.headers
         // // console.log('request.headers["Access-Control-Allow-Origin"]', request.headers["Access-Control-Allow-Origin"]);
         // // config//headers["Access-Control-Allow-Credentials","true"]
         // // 启用此项后，上面的域名不能为'*'，必须指定具体的域名，否则浏览器会提示错误
         // // request.headers["Access-Control-Allow-Credentials"] = true;
-        // request.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE, PATCH";
-        // request.headers["Access-Control-Max-Age"] = 3600;
-        // request.headers["Access-Control-Allow-Headers"]
-        //     = " X-Requested-With, Content-Type";// token,Origin,, Accept,mid,X-Token
-        // request.headers["Content-Type"] = "application/json;charset=utf-8";
-        // console.log('request.headers["Content-Type"]',request.headers["Content-Type"])
 
+        request.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE, PATCH";
+        // request.headers["Access-Control-Max-Age"] = 3600;
+        // request.headers["access-control-allow-headers"]
+        //     = " X-Requested-With, Content-Type,Origin,";// token, Accept,mid,X-Token
+        request.headers["Content-Type"] = "application/json";
+        // console.log('request.headers["Content-Type"]',request.headers["Content-Type"])
+        // console.log('request headers["Access-Control-Allow-Origin"]:', request.headers["Access-Control-Allow-Origin"]);
         // console.log('request.headers["Access-Control-Allow-Origin"]-after', request.headers["Access-Control-Allow-Origin"]);
         return request
-    },error=>{
-        console.log(error)
+    }, error => {
+        console.error('request error:',error)
     })
 
     axios.interceptors.response.use(response => {
@@ -32,14 +34,17 @@ const setAxios = () => {
         // response.headers["Access-Control-Max-Age"] = 3600;
         // response.headers["Access-Control-Allow-Headers"]
         //     = "token,Origin, X-Requested-With, Content-Type, Accept,mid,X-Token";
-        // response.headers["Access-Control-Allow-Origin"] = "*";
+        console.log('axios-response-set:', response)
+        // response.headers["content-type"] = "application/json";
+        // response.headers["access-control-allow-origin"] = "*";
+        // response.headers['Access-Control-Allow-Origin']= '*';
 
         return response
-    },error=>{
-        console.log(error)
+    }, error => {
+        console.error('response error:',error)
     })
 
-    
+
 }
 
 export { setAxios }
@@ -57,7 +62,7 @@ export { setAxios }
 // });
 
 // instance.interceptors.response.use(function(response){
-    
+
 //     return response.data;
 // },function(error){
 //     return Promise.reject(error);
