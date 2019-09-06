6f8a9b1afe979a252d8b21d8af9e20dd9f79ce87
kevinjang <kevinjang@163.com>
2019-09-06 17:34:50 +0800

    Create spnningTest.js
    等待测试模块。

---
+++ src/pages/compoTests/spnningTest.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Spin} from 'antd'
+
+class SpinningTest extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    state = {
+        spinning: true
+    }
+
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({
+                spinning: false
+            })
+        }, 5000);
+    }
+
+    render(){
+        return (
+            <Spin tip='Loading...' spinning={this.state.spinning}>
+                <div style={{width: '100%', height: 'calc(100vh - 100px)', backgroundColor: 'violet'}}>
+
+                </div>
+            </Spin>
+        )
+    }
+}
+
+export default SpinningTest
