00bafbefd248bf67626cf7efdb42604c1aa399a3
kevinjang <kevinjang@163.com>
2019-09-11 22:41:31 +0800

    Update BasePeopleUtil.js
    新增更新方法。

--- src/utils/toserver/BasePeopleUtil.js
+++ src/utils/toserver/BasePeopleUtil.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {message} from 'antd'
+import { message } from 'antd'
 function insert(record, callback) {
     axios.post('/insertBasePeople', {
         headers: {
@@ -10,14 +10,14 @@ function insert(record, callback) {
         responseType: 'json'
     }).then((response) => {
         console.log('insert result:', response.data.result.message)
-        if(response && response.data && response.data.result && response.data.result.message){
+        if (response && response.data && response.data.result && response.data.result.message) {
             message.success(response.data.result.message)
         }
         else {
             message.error(response.statusText);
         }
 
-        if(callback){
+        if (callback) {
             callback();
         }
     }).catch(err => {
@@ -26,4 +26,30 @@ function insert(record, callback) {
     })
 }
 
-export { insert }
+function update(record, callback) {
+    axios.post('/updateBasePeople', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        params: { record },
+        responseType: 'json'
+    }).then((response) => {
+        console.log('update result:', response.data.result.message)
+        if (response && response.data && response.data.result && response.data.result.message) {
+            message.success(response.data.result.message)
+        }
+        else {
+            message.error(response.statusText);
+        }
+
+        if (callback) {
+            callback();
+        }
+    }).catch(err => {
+        if (err)
+            console.log('insertBasePeople-error:', err);
+    })
+}
+
+export { insert, update }
