c342dbcc48add3d4f22a063a7b70a6ad045d815c
kevinjang <kevinjang@163.com>
2019-09-26 17:34:06 +0800

    Update EmployeeBPMaintain.js
    加上弹出界面。加入与服务器交互的方法。加入备注列。格式化。

--- src/pages/BPM_RM/EmployeeBPMaintain.js
+++ src/pages/BPM_RM/EmployeeBPMaintain.js
@@ -1,18 +1,24 @@
 
 // import styles from './EmployeeBPMaintain.css';
 import React from 'react'
-import { Spin, Row, Col, Layout, Table, Form, Button, Input, Icon, Select, Popconfirm, message } from 'antd'
+import {
+  Spin, Row, Col, Layout, Table, Form, Button, Input,
+  Icon, Select, Popconfirm, message, Modal
+} from 'antd'
+
+import EmployeeBPMaintainItemModalFormComp from './EmployeeBPMaintainItemModal'
 
 const { Header } = Layout;
 const { Option } = Select;
 
-import { getByPage } from '../../utils/toserver/EmployeeBPUtil'
+import { getByPage, insert, update, deleteItem, deleteItems } from '../../utils/toserver/EmployeeBPUtil'
 
 class EmployeeBPMaintain extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      modalShow: false,
       spinning: true,
       dataSource: [],
       filterCol: '-',
@@ -20,6 +26,9 @@ class EmployeeBPMaintain extends React.Component {
       pagi_pageSize: 10,
       pagi_total: 0,
       pagi_current: 0,
+      operation: '',
+      // 编辑项
+      editingRecord: null
     }
 
     this.pagination = {
@@ -52,7 +61,8 @@ class EmployeeBPMaintain extends React.Component {
       {
         key: 'EmployeeName',
         dataIndex: 'EmployeeName',
-        title: '姓名'
+        title: '姓名',
+        width: '100px'
       },
       {
         key: 'PriDept',
@@ -62,12 +72,14 @@ class EmployeeBPMaintain extends React.Component {
       {
         key: 'SecDept',
         dataIndex: 'SecDept',
-        title: '二级部门'
+        title: '二级部门',
+        width: '100px'
       },
       {
         key: 'BankAccount',
         dataIndex: 'BankAccount',
-        title: '银行账号'
+        title: '银行账号',
+        width: '100px'
       },
       {
         key: 'BankName',
@@ -78,6 +90,11 @@ class EmployeeBPMaintain extends React.Component {
         key: 'Email',
         dataIndex: 'Email',
         title: 'Email'
+      },
+      {
+        key: 'Remark',
+        dataIndex: 'Remark',
+        title: '备注'
       },
       {
         // key: 'BPNo',
@@ -98,6 +115,10 @@ class EmployeeBPMaintain extends React.Component {
       }
     ];
 
+    this.form = this.props.form;
+
+    // console.log('my form:', this.form);
+
     this.options = this.columns.filter(item => (item.key !== 'RowNum' && item.title !== '操作'))
       .map(item => {
         return <Option value={item.key} key={item.key} >
@@ -105,7 +126,7 @@ class EmployeeBPMaintain extends React.Component {
         </Option>
       });
 
-    this.options.unshift(<Option value="-" key="-">请选择</Option>);
+    this.options.unshift(<Option value="-" key="-" >请选择</Option>);
   }
 
   handleSearch = (e) => {
@@ -113,10 +134,82 @@ class EmployeeBPMaintain extends React.Component {
   }
 
   handleEditRecord = (e) => {
+    // console.log('edit-e:', e);
+    this.setState({
+      editingRecord: e,
+      modalShow: true,
+      operation: 'update'
+    })
+  }
 
+  handleAddRecord = () => {
+    var record = {
+      RowNum: this.state.dataSource.length + 1,
+      BPNo: '',
+      EmployeeName: '',
+      PriDept: '',
+      SecDept: '',
+      BankAccount: '',
+      BankName: '',
+      Email: '',
+      Remark: ''
+    }
+
+    this.setState({
+      editingRecord: record,
+      modalShow: true,
+      operation: 'insert'
+    })
   }
 
   handleDeleteRecord = (e) => {
+    console.log('delete-e:', e);
+    const { ID } = e;
+    deleteItem(ID, this.loadData);
+  }
+
+  handleDeleteSelectedRecords = () => {
+    const { selectedRowKeys } = this.state;
+    if (!selectedRowKeys || selectedRowKeys.length === 0) {
+      message.info('请选择要删除的记录！');
+      return;
+    }
+
+    Modal.confirm({
+      centered: true,
+      content: '确认删除已选中的行吗？',
+      cancelText: '放弃删除',
+      okText: '确认删除',
+      onCancel: () => {
+        return;
+      },
+      onOk: () => {
+
+        var toDeleteItemsIDs = [];
+        var toDeleteItems = this.state.dataSource.filter(it => selectedRowKeys.includes(it.key));
+
+        toDeleteItems.forEach(it => {
+          toDeleteItemsIDs.push(it.ID);
+        });
+
+        deleteItems(toDeleteItemsIDs, this.loadData).then(response => {
+          if (response && response.data && response.data.result && response.data.result.message) {
+            message.success(response.data.result.message)
+            this.setState({
+              selectedRowKeys: []
+            })
+          }
+          else {
+            message.error(response.statusText);
+          }
+
+          this.loadData();
+        }).catch(err => {
+          if (err)
+            console.log('delete multiple item error:', err);
+        });
+      }
+    })
 
   }
 
@@ -126,6 +219,17 @@ class EmployeeBPMaintain extends React.Component {
       filterCol: e
     }, () => {
       console.log(this.state.filterCol)
+    })
+  }
+
+  onTableRowSelectedChange = (selectedRowKeys) => {
+    // console.log('onTableRowSelectedChange-selectedRowKeys:', selectedRowKeys);
+    this.setState({ selectedRowKeys })
+  }
+
+  onModalOK = () => {
+    this.setState({
+      modalShow: false
     })
   }
 
@@ -165,53 +269,113 @@ class EmployeeBPMaintain extends React.Component {
   }
 
   render() {
-    // const { getFieldDecorator } = this.props.form;
-        const tableFooter = () => {
-          return `共计${this.state.allCount}条数据`
-      }
+    const tableFooter = () => {
+      return `共计${this.state.allCount}条数据`
+    }
+
+    const { getFieldDecorator } = this.form;
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onTableRowSelectedChange
+    }
+
     return <Spin spinning={this.state.spinning}>
       <div>
         <Header style={{ backgroundColor: 'whitesmoke' }}>
           <Form>
             <Form.Item style={{ float: 'right' }}>
-              <Button type="primary">添加</Button>
+              {
+                getFieldDecorator('ebm_add_btn')(
+                  <Button type="primary" onClick={this.handleAddRecord}>添加</Button>
+                )
+              }
             </Form.Item>
             <Form.Item style={{ float: 'right' }}>
-              <Button type='danger'>删除所选</Button>
+              {
+                getFieldDecorator('ebm_delete_btn')(
+                  <Button type='danger' onClick={this.handleDeleteSelectedRecords}>删除所选</Button>)
+              }
             </Form.Item>
             <Form.Item style={{ float: 'right' }}>
-              <Input suffix={<a href="" onClick={this.handleSearch}><Icon type='search' /></a>} ></Input>
+              {
+                getFieldDecorator('ebm_filter_text')(
+                  <Input suffix={<a href="" onClick={this.handleSearch}><Icon type='search' /></a>} ></Input>
+                )
+              }
             </Form.Item>
             <Form.Item style={{ float: 'right' }}>
-              <Select style={{ minWidth: '100px' }} onChange={this.onFilterSelectChange}>
-                {this.options}
-              </Select>
+              {
+                getFieldDecorator('ebm_filter_combo')(
+                  <Select style={{ minWidth: '100px' }} onChange={this.onFilterSelectChange}>
+                    {this.options}
+                  </Select>
+                )
+              }
             </Form.Item>
           </Form>
         </Header>
         <Layout>
-          <Table
-            columns={this.columns}
-            bordered
-            footer={tableFooter}
-            dataSource={this.state.dataSource}
-            pagination={this.pagination}>
+          <Form>
+            <Form.Item>
+              <Table
+                columns={this.columns}
+                bordered
+                footer={tableFooter}
+                dataSource={this.state.dataSource}
+                pagination={this.pagination}
+                rowSelection={rowSelection}
+                onRow={
+                  (record, index) => {
+                    return {
+                      onDoubleClick: (event) => {
+                        this.setState({
+                          modalShow: true,
+                          editingRecord: record,
+                          operation: 'update'
+                        })
+                      }
+                    }
+                  }
+                }>
 
-          </Table>
+              </Table>
+            </Form.Item>
+          </Form>
         </Layout>
       </div>
+      <Modal
+        visible={this.state.modalShow}
+        title="编辑项目"
+        maskClosable={false}
+        destroyOnClose={true}
+        centered={true}
+        onOk={this.onModalOK}
+        onCancel={
+          () => {
+            this.setState({
+              modalShow: false
+            })
+          }
+        }>
+        <EmployeeBPMaintainItemModalFormComp
+          editingRecord={this.state.editingRecord}
+          operation={this.state.operation}>
+
+        </EmployeeBPMaintainItemModalFormComp>
+      </Modal>
     </Spin>
   }
 }
 
-const EmployeeBPMaintainForm = Form.create({name: 'employee_bp_maintain'})(EmployeeBPMaintain);
+const EmployeeBPMaintainForm = Form.create({ name: 'employee_bp_maintain' })(EmployeeBPMaintain);
 
-class EmployeeBPMaintainFormComp extends React.Component{
-  constructor(props){
+class EmployeeBPMaintainFormComp extends React.Component {
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     return (
       <EmployeeBPMaintainForm {...this.props}></EmployeeBPMaintainForm>
     );
