c14c3cf9c6468dc6524454918e6601f86c56eb84
kevinjang <kevinjang@163.com>
2019-09-26 17:32:53 +0800

    Update EmployeeBPUtil.js
    创建中间层，与服务器端进行交互。

--- src/utils/toserver/EmployeeBPUtil.js
+++ src/utils/toserver/EmployeeBPUtil.js
@@ -45,6 +45,99 @@ function getByPage(pageSize, startPage, condition, callback) {
     });
 }
 
+function insert(record, callback) {
+    axios.post('/insertEmployeeBP', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        params: {
+            record
+        },
+        responseType: 'json'
+    }).then(response => {
+        if (response && response.data && response.data.result && response.data.result.message) {
+            message.success(response.data.result.message)
+        }
+        else {
+            message.error(response.statusText);
+        }
 
-export { getByPage }
+        if (callback) {
+            callback();
+        }
+    }).catch(err => {
+        if (err)
+            console.log('insertBasePeople-error:', err);
+    })
+}
+
+function update(record, callback) {
+    axios.post('/updateEmployeeBP', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        params: { record },
+        responseType: 'json'
+    }).then((response) => {
+        console.log('update result:', response.data.result.message)
+        if (response && response.data && response.data.result && response.data.result.message) {
+            message.success(response.data.result.message)
+        }
+        else {
+            message.error(response.statusText);
+        }
+
+        if (callback) {
+            callback();
+        }
+    }).catch(err => {
+        if (err)
+            console.log('insertBasePeople-error:', err);
+    })
+}
+
+function deleteItem(ID, callback) {
+    axios.post('/employeeBPDeleteSingle', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/json'
+        },
+        params: { ID },
+        responseType: 'json'
+    }).then(response => {
+        if (response &&
+            response.data &&
+            response.data.result &&
+            response.data.result.message) {
+            message.success(response.data.result.message);
+        }
+        else {
+            message.error(response.statusText)
+        }
+
+        if (callback) {
+            callback();
+        }
+    }).catch(err => {
+        if (err)
+            console.log('delete single item error:', err);
+    })
+}
+
+async function deleteItems(IDs, callback) {
+    return axios.post('/employeeBPDeleteMultiple', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        params: {
+            IDs
+        },
+        responseType: 'json'
+    });
+}
+
+export { getByPage, insert, update, deleteItem, deleteItems }
 // , insert, update, deleteItem, deleteItems
