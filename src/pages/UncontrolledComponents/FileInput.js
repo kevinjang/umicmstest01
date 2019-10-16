b787c92ef277ab9b4fd5f6b8b837e6b91c40e14f
kevinjang <kevinjang@163.com>
2019-10-16 19:37:18 +0800

    Create FileInput.js

---
+++ src/pages/UncontrolledComponents/FileInput.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class FileInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fileinput = React.createRef();
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        alert(
+            `Selected File ${this.fileinput.current.files[0].name}`
+        );
+    }
+
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label >
+                    Upload File:
+                    <input type="file" ref={this.fileinput}/>
+                </label>
+                <br/>
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default FileInput
