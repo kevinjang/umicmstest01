fd47e6f452c5d8571eb057974eabf22a8908dd79
kevinjang <kevinjang@163.com>
2019-10-18 16:47:48 +0800

    Update ListItem.js
    使用Tag作为listitem的extra信息增加效果展示。

--- src/pages/Notification/ListItem.js
+++ src/pages/Notification/ListItem.js
@@ -1,17 +1,27 @@
 import React from 'react'
 
-import { List, Avatar, Button } from 'antd'
+import { List, Avatar, Button, Tag, message } from 'antd'
+
+import styles from './ListItem.css'
 
 class ListItem extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    onTagClicked = (item) =>{
+        console.log('onTagClicked-item:',item)
+        if(this.props.callSetState){
+
+            this.props.callSetState(item);
+        }
+    }
+
     render() {
         return (
             <div>
                 <List split={true} style={{ cursor: 'pointer' }}
-                    
+
                     loadMore={
                         <Button style={{ width: '100%', marginTop: '5px' }} type="default">
                             加载更多
@@ -20,8 +30,23 @@ class ListItem extends React.Component {
                     dataSource={this.props.dataSource}
                     itemLayout="horizontal"
                     renderItem={(item) => {
+                        return <List.Item extra={item.type === 'todo' ? (
+                        <Tag 
+                            color={item.completed ? "red" : "#87d068"} //style={{ float: 'right' }} 
+                            onClick={(e)=>{
+                                // message.info('tag clicked');
+                                console.log('tag-item:', item);
+                                const itemNew = {
+                                    ...item, 
+                                    completed: !item.completed
+                                }
+                                this.onTagClicked(itemNew);
+                            }}
+                            >
+                            {item.completed ? '未完成' : '已完成'}
+                        </Tag>
+                        ) : ''}>
 
-                        return <List.Item>
                             <List.Item.Meta avatar={<Avatar
                                 src={item.avatar ? item.avatar : ''}
                                 icon={item.icon ? item.icon : ''}></Avatar>}
