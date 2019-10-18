7c3577bfa1225e20c40e79ae31b7fad9cd59f31b
kevinjang <kevinjang@163.com>
2019-10-18 17:32:45 +0800

    Update ListItem.js
    规范代码格式；

--- src/pages/Notification/ListItem.js
+++ src/pages/Notification/ListItem.js
@@ -9,9 +9,9 @@ class ListItem extends React.Component {
         super(props);
     }
 
-    onTagClicked = (item) =>{
-        console.log('onTagClicked-item:',item)
-        if(this.props.callSetState){
+    onTagClicked = (item) => {
+        console.log('onTagClicked-item:', item)
+        if (this.props.callSetState) {
 
             this.props.callSetState(item);
         }
@@ -23,28 +23,39 @@ class ListItem extends React.Component {
                 <List split={true} style={{ cursor: 'pointer' }}
 
                     loadMore={
-                        <Button style={{ width: '100%', marginTop: '5px' }} type="default">
+                        // <Button style={{ width: '100%', marginTop: '5px', position: 'absolute', marginBottom: '0px' }} type="default">
+                        //     加载更多
+                        //         </Button>
+                        <div style={{
+                            width: '100%',
+                            marginTop: '5px',
+                            textAlign: 'center',
+                            padding: '10px 0',
+                            borderBottom: '1px solid lightgray',
+                            borderTop: '1px solid lightgray',
+                            borderRadius: '4px'
+                        }} >
                             加载更多
-                                </Button>
+                        </div>
                     }
                     dataSource={this.props.dataSource}
                     itemLayout="horizontal"
                     renderItem={(item) => {
                         return <List.Item extra={item.type === 'todo' ? (
-                        <Tag 
-                            color={item.completed ? "red" : "#87d068"} //style={{ float: 'right' }} 
-                            onClick={(e)=>{
-                                // message.info('tag clicked');
-                                console.log('tag-item:', item);
-                                const itemNew = {
-                                    ...item, 
-                                    completed: !item.completed
-                                }
-                                this.onTagClicked(itemNew);
-                            }}
+                            <Tag
+                                color={item.completed ? "red" : "#87d068"} //style={{ float: 'right' }} 
+                                onClick={(e) => {
+                                    // message.info('tag clicked');
+                                    console.log('tag-item:', item);
+                                    const itemNew = {
+                                        ...item,
+                                        completed: !item.completed
+                                    }
+                                    this.onTagClicked(itemNew);
+                                }}
                             >
-                            {item.completed ? '未完成' : '已完成'}
-                        </Tag>
+                                {item.completed ? '未完成' : '已完成'}
+                            </Tag>
                         ) : ''}>
 
                             <List.Item.Meta avatar={<Avatar
