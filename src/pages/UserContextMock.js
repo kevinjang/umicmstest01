58292328d1bd80ea448ef41b3a3ff4698462b3f1
kevinjang <kevinjang@163.com>
2019-10-18 16:43:49 +0800

    Update UserContextMock.js
    查询用户数据，然后创建context。

--- src/pages/UserContextMock.js
+++ src/pages/UserContextMock.js
@@ -1,20 +1,38 @@
 import React from 'react'
 
-export const UserOUInfo = {
-    UserInfo : {
-        FirstName: 'Kevin',
-        SurName: 'Jang'
-    },
-    Department:{
-        FirstDept:{
-            Name: '财务部'
-        },
-        Dept:{
-            Name: 'IT小组'
-        }
+import { getUserByPage } from '../utils/toserver/UserBaseInfoUtil'
+
+let UserContext = null;
+let MyUserData = null;
+
+function GetData(userAD, cb) {
+    if (!userAD.startsWith('cofco\\')) {
+        userAD = "cofco\\" + userAD;
     }
+
+    getUserByPage(0, 1, {
+        name: 'UserAD',
+        value: userAD
+    }, (data) => {
+        UserContext = React.createContext(data);
+        MyUserData = data;
+        if (cb) cb();
+    })
 }
 
-export const UserContext = React.createContext({
-    UserOUInfo
-});
+// export const UserOUInfo = {
+//     UserInfo: {
+//         FirstName: 'Kevin',
+//         SurName: 'Jang'
+//     },
+//     Department: {
+//         FirstDept: {
+//             Name: '财务部'
+//         },
+//         Dept: {
+//             Name: 'IT小组'
+//         }
+//     }
+// }
+
+export { UserContext, GetData, MyUserData } 
