2dbdaa31763a3624437ef22f67051bdf504c1fc0
kevinjang <kevinjang@163.com>
2019-10-18 16:43:11 +0800

    Create UserBaseInfoUtil.js
    增加用户登录查询。

---
+++ src/utils/toserver/UserBaseInfoUtil.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import { message } from 'antd'
+var baseURL = axios.defaults.baseURL = "http://localhost:3000";
+
+function getUserByPage(pageSize, startPage, condition, callback) {
+    axios.get(baseURL + '/getUserBaseInfo', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/json'
+        },
+        params: { pageSize, startPage, condition },
+        responseType: 'json'
+    }).then((response) => {
+        console.log('get-response-data:', response.data);
+        var results = response.data.results;
+        results = results.map((item, index) => {
+            return {
+                key: item.new_id,
+                RowNum: item.new_id,
+                ...item,
+                valid: 'valid'
+            }
+        });
+
+        callback({
+            PaginationTotal: parseInt(response.data.allCount) || 0,
+            dataSource: results,
+            allCount: response.data.allCount,
+            pagi_total: response.data.allCount,
+            spinning: false
+        })
+        if (response.data.message === 'succeeded') {
+            // message.success('离职查询授权-加载成功')
+        }
+        else {
+            message.error(response.data.message)
+        }
+    }).catch((err) => {
+        console.log({ ...err })
+        callback({
+            // spinning: false
+        })
+        message.error(err.message);
+    });
+}
+
+export { getUserByPage }
