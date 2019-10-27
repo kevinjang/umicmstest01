38648ffec539c04f6d0981d4f5bb9a0af575572c
kevinjang <kevinjang@163.com>
2019-10-27 22:27:20 +0800

    Update UserBaseInfoUtil.js
    重构函数，重命名。

--- src/utils/toserver/UserBaseInfoUtil.js
+++ src/utils/toserver/UserBaseInfoUtil.js
@@ -2,31 +2,32 @@ import axios from 'axios'
 import { message } from 'antd'
 var baseURL = axios.defaults.baseURL = "http://localhost:3000";
 
-function getUserByPage(pageSize, startPage, condition, callback) {
-    axios.get(baseURL + '/getUserBaseInfo', {
+async function getUserBaseInfoByAD(userAD, callback) {
+    axios.get(baseURL + '/getUserBaseInfoByAD', {
         headers: {
             "Access-Control-Allow-Origin": "http://localhost:3000",
             'Content-Type': 'application/json'
         },
-        params: { pageSize, startPage, condition },
+        params: { userAD },
         responseType: 'json'
     }).then((response) => {
         console.log('get-response-data:', response.data);
-        var results = response.data.results;
-        results = results.map((item, index) => {
-            return {
-                key: item.new_id,
-                RowNum: item.new_id,
-                ...item,
-                valid: 'valid'
-            }
-        });
+        var row = response.data.row;
+        // results = results.map((item, index) => {
+        //     return {
+        //         key: item.new_id,
+        //         RowNum: item.new_id,
+        //         ...item,
+        //         valid: 'valid'
+        //     }
+        // });
 
         callback({
-            PaginationTotal: parseInt(response.data.allCount) || 0,
-            dataSource: results,
-            allCount: response.data.allCount,
-            pagi_total: response.data.allCount,
+            // PaginationTotal: parseInt(response.data.allCount) || 0,
+            // dataSource: results,
+            // allCount: response.data.allCount,
+            // pagi_total: response.data.allCount,
+            userRow: row,
             spinning: false
         })
         if (response.data.message === 'succeeded') {
@@ -38,10 +39,10 @@ function getUserByPage(pageSize, startPage, condition, callback) {
     }).catch((err) => {
         console.log({ ...err })
         callback({
-            // spinning: false
+            spinning: false
         })
         message.error(err.message);
     });
 }
 
-export { getUserByPage }
+export { getUserBaseInfoByAD }
