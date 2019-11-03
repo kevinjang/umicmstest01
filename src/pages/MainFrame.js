10bd09c4ef01e876075834143ddfd7817c114d69
kevinjang <kevinjang@163.com>
2019-11-03 12:21:57 +0800

    Update MainFrame.js
    自定义滚动条。

--- src/pages/MainFrame.js
+++ src/pages/MainFrame.js
@@ -15,6 +15,8 @@ import Notification from './Notification/Notification'
 
 import { UserContext, GetData, MyUserData } from './UserContextMock';
 
+import { Scrollbars } from 'react-custom-scrollbars'
+
 // const UserContextMock = UserContext
 
 @connect(
@@ -130,30 +132,32 @@ class KLayout extends React.Component {
                                 style={{ minHeight: '91.5vh', color: 'white', paddingTop: 16 }}
                                 theme={this.state.theme}
                                 collapsible>
-                                <Menu
-                                    theme={this.state.theme}
-                                    defaultSelectedKeys={this.state.selectedKeys}
-                                    defaultOpenKeys={['sub1']}
-                                    mode='inline'
-                                    inlineCollapsed={this.state.collapsed}
-                                >
-                                    {this.props.menus.map(item => {
-                                        if (item.children)
-                                            return <SubMenu key={item.id} title={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
-                                                {item.children.map(cItem => {
-                                                    return <Item key={cItem.id + '_' + cItem.nodeInfo} onClick={this.menuItemClick}>
-                                                        <Icon type={cItem.icon}></Icon>
-                                                        <span>{cItem.title}</span>
-                                                    </Item>
-                                                })}
-                                            </SubMenu>
-                                        else
-                                            return <Item key={item.id}>
-                                                <Icon type={item.icon}></Icon>
-                                                <span>{item.title}</span>
-                                            </Item>
-                                    })}
-                                </Menu>
+                                <Scrollbars>
+                                    <Menu
+                                        theme={this.state.theme}
+                                        defaultSelectedKeys={this.state.selectedKeys}
+                                        defaultOpenKeys={['sub1']}
+                                        mode='inline'
+                                        inlineCollapsed={this.state.collapsed}
+                                    >
+                                        {this.props.menus.map(item => {
+                                            if (item.children)
+                                                return <SubMenu key={item.id} title={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
+                                                    {item.children.map(cItem => {
+                                                        return <Item key={cItem.id + '_' + cItem.nodeInfo} onClick={this.menuItemClick}>
+                                                            <Icon type={cItem.icon}></Icon>
+                                                            <span>{cItem.title}</span>
+                                                        </Item>
+                                                    })}
+                                                </SubMenu>
+                                            else
+                                                return <Item key={item.id}>
+                                                    <Icon type={item.icon}></Icon>
+                                                    <span>{item.title}</span>
+                                                </Item>
+                                        })}
+                                    </Menu>
+                                </Scrollbars>
                             </Sider>
                             <Layout>
                                 <Content style={{ marginTop: '16px', marginLeft: '16px', marginRight: '16px', maxHeight: "90vh - 10px", overflow: 'overlay' }}>
@@ -175,7 +179,7 @@ class KLayout extends React.Component {
                                                         forceRender={false}
                                                         closable={true}>
                                                         <Content >
-                                                            {console.log('item.nodeInfo:',item.nodeInfo)}
+                                                            {console.log('item.nodeInfo:', item.nodeInfo)}
                                                             <Route component={this.loadTest(item.nodeInfo, {
                                                                 activeKey: this.state.activeTabKey,
                                                                 selfID: item.id
