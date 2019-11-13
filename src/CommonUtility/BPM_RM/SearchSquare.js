c90df3abc05d06d9fe924c172e4e2c9e5db75542
kevinjang <kevinjang@163.com>
2019-11-13 17:26:29 +0800

    Update SearchSquare.js
    更新样式。

--- src/CommonUtility/BPM_RM/SearchSquare.js
+++ src/CommonUtility/BPM_RM/SearchSquare.js
@@ -14,11 +14,16 @@ export default function (props) {
     // const {FormItems} = props.items;
     return (
         <div>
-            <Header style={{ backgroundColor: 'whitesmoke' }}>
+            <Header style={{
+                backgroundColor: 'whitesmoke'
+                // , padding: '-10px 0'
+                // , border: '1px solid black'
+                ,height: '40px'
+            }}>
                 <Form>
                     {props.items ?
                         props.items.map((item) => {
-                            return <Form.Item style={{ float: 'right' }}>
+                            return <Form.Item style={{ float: 'right', marginBottom: '0px' }}>
                                 {
                                     getFieldDecorator(item.name)(item.obj)
                                 }
