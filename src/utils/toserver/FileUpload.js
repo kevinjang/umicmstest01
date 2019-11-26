af200472b3cd58bf9e911c8f36d8f2b0b6648ca2
kevinjang <kevinjang@163.com>
2019-11-26 17:30:01 +0800

    Update FileUpload.js
    增加了file.name后上传成功。

--- src/utils/toserver/FileUpload.js
+++ src/utils/toserver/FileUpload.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import qs from 'querystring'
 var baseURL = axios.defaults.baseURL = "http://localhost:3000";
 
-async function upload(file) {
+async function upload(file, callback) {
     let formData = new FormData();
-    formData.append('avatar', file);
+    formData.append('avatar', file, file.name);
     console.log('qs:', qs)
     return await axios.post('/fileupload', formData, {
         headers: {
@@ -17,6 +17,10 @@ async function upload(file) {
         responseType: 'json'
     }).then(response => {
         console.log('file upload response:', response)
+        const message = response.data.message;
+        callback({
+            message
+        });
     }).catch(error => {
         console.log('upload error:', error)
     })
