fe6fbe65b1ed7e6983cb7556ce9830004fa60ff7
kevinjang <kevinjang@163.com>
2019-11-27 23:13:52 +0800

    Update DownloadFile.js

--- src/utils/toserver/DownloadFile.js
+++ src/utils/toserver/DownloadFile.js
@@ -8,30 +8,34 @@ async function download(url) {
     return axios.post('/DownloadFile', {
         headers: {
             "Access-Control-Allow-Origin": "http://localhost:3000",
-            'Content-Type': 'blob'
+            'Content-Type': 'application/octet-stream'
         },
         // params:{
         //     fileData: file
         // },
-        // responseType: 'json'
+        responseType: 'blob'
     }).then(response => {
-        console.log('download-Buffer(response.data):', response.data);
-        // const readable = fs.createReadStream(Buffer(response.data));
-        // console.log('atob response.data:', window.atob(response.data))
+        // console.log('response.data:', response.data)
 
-        // var reader = new FileReader();
-        // reader.readAsBinaryString(new Blob([response]));
-        // reader.onload = function(e){
-        //     console.log('FileReader-result:',e.target.result)
+        // const blob = response.data;
+        // const reader = new FileReader();
+        // reader.readAsDataURL(blob);
+        // reader.onload=function(e){
+        //     const a = document.createElement('a');
+        //     a.download = Date.now()+".jpg";
+        //     a.href = e.target.result;
+        //     document.body.appendChild(a);
+        //     a.click();
+        //     document.body.removeChild(a);
         // }
 
+        // var src = "data:image/jpg;base64,"+btoa(new Uint8Array(response.data).reduce((data, byte)=>data+String.fromCharCode(byte), ''));
 
         var eleLink = document.createElement('a');
         eleLink.download = Date.now() + ".jpg";
         eleLink.style.display = 'none';
-        var blob = new Blob([Buffer(response.data)],{
-            type: 'image/jpeg'
-        });
+        var blob = new Blob([(response.data)]);
+        // console.log('src:', src);
         eleLink.href = URL.createObjectURL(blob);
         document.body.appendChild(eleLink);
         eleLink.click();
