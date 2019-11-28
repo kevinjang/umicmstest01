c240076c906c312c2fa7f1a18ad3d2cb7c6d8d6a
kevinjang <kevinjang@163.com>
2019-11-28 16:51:05 +0800

    Update DownloadFile.js
    改用xhr方式下载文件，现在只有图片，其他文件待确认。

--- src/utils/toserver/DownloadFile.js
+++ src/utils/toserver/DownloadFile.js
@@ -1,49 +1,74 @@
 import axios from 'axios';
 import { message } from 'antd'
-import ReactDOM from 'react-dom'
-import fs from 'fs'
 var baseURL = axios.defaults.baseURL = "http://localhost:3000";
 
 async function download(url) {
-    return axios.post('/DownloadFile', {
-        headers: {
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-            'Content-Type': 'application/octet-stream'
-        },
-        // params:{
-        //     fileData: file
-        // },
-        responseType: 'blob'
-    }).then(response => {
-        // console.log('response.data:', response.data)
+    // axios.post('/DownloadFile', {
+    //     headers: {
+    //         "Access-Control-Allow-Origin": "http://localhost:3000",
+    //         'Content-Type': 'multipart/form-data'
+    //     },
+    //     // params:{
+    //     //     fileData: file
+    //     // },
+    //     responseType: 'arraybuffer'
+    // }).then(response => {
+    //     console.log('response:', response);
 
-        // const blob = response.data;
-        // const reader = new FileReader();
-        // reader.readAsDataURL(blob);
-        // reader.onload=function(e){
-        //     const a = document.createElement('a');
-        //     a.download = Date.now()+".jpg";
-        //     a.href = e.target.result;
-        //     document.body.appendChild(a);
-        //     a.click();
-        //     document.body.removeChild(a);
-        // }
+    //     // const blob = response.data;
+    //     // const reader = new FileReader();
+    //     // reader.readAsDataURL(blob);
+    //     // reader.onload=function(e){
+    //     //     const a = document.createElement('a');
+    //     //     a.download = Date.now()+".jpg";
+    //     //     a.href = e.target.result;
+    //     //     document.body.appendChild(a);
+    //     //     a.click();
+    //     //     document.body.removeChild(a);
+    //     // }
 
-        // var src = "data:image/jpg;base64,"+btoa(new Uint8Array(response.data).reduce((data, byte)=>data+String.fromCharCode(byte), ''));
+    //     // var src = "data:image/jpg;base64,"+btoa(new Uint8Array(response.data).reduce((data, byte)=>data+String.fromCharCode(byte), ''));
 
-        var eleLink = document.createElement('a');
-        eleLink.download = Date.now() + ".jpg";
-        eleLink.style.display = 'none';
-        var blob = new Blob([(response.data)]);
-        // console.log('src:', src);
-        eleLink.href = URL.createObjectURL(blob);
-        document.body.appendChild(eleLink);
-        eleLink.click();
-        document.body.removeChild(eleLink);
-        window.URL.revokeObjectURL(blob)
-    }).catch(err => {
-        message.error(err.message)
-    })
+    //     var eleLink = document.createElement('a');
+    //     eleLink.download = Date.now() + ".txt";
+    //     eleLink.style.display = 'none';
+    //     var blob = new Blob([response.data],{
+    //         type: 'plain/text'
+    //     });
+    //     // console.log('src:', src);
+    //     eleLink.href = URL.createObjectURL(blob);
+    //     document.body.appendChild(eleLink);
+    //     eleLink.click();
+    //     document.body.removeChild(eleLink);
+    //     window.URL.revokeObjectURL(blob)
+    // }).catch(err => {
+    //     message.error(err.message)
+    // })
+
+    var xhr = new XMLHttpRequest();
+    // xhr.open('POST', baseURL + '/DownloadFile', true);
+    xhr.open('GET', 'http://e.hiphotos.baidu.com/image/pic/item/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg', true);
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var content = this.response;
+            console.log('this:', this);
+            var a = document.createElement('a');
+            // var blob = new Blob([content]);
+            // var headerName = xhr.getResponseHeader('Content-Disposition');
+            var fileName = Date.now() + ".jpg";// decodeURIComponent(headerName).substring(20);
+            // console.log('fileName:', fileName);
+            a.download = fileName;
+            a.href = URL.createObjectURL(content);
+            a.click();
+            URL.revokeObjectURL(content);
+            message.success('下载成功');
+        }
+    }
+    xhr.onerror = () => {
+        message.error(this.statusText);
+    }
+    xhr.send();
 }
 
 export { download }
