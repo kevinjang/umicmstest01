e8765343e86a68218f4c7a8d3f6db5cf33d70930
kevinjang <kevinjang@163.com>
2019-11-29 16:24:13 +0800

    Update DownloadFile.js
    xhr下载图片。

--- src/utils/toserver/DownloadFile.js
+++ src/utils/toserver/DownloadFile.js
@@ -3,51 +3,10 @@ import { message } from 'antd'
 var baseURL = axios.defaults.baseURL = "http://localhost:3000";
 
 async function download(url) {
-    // axios.post('/DownloadFile', {
-    //     headers: {
-    //         "Access-Control-Allow-Origin": "http://localhost:3000",
-    //         'Content-Type': 'multipart/form-data'
-    //     },
-    //     // params:{
-    //     //     fileData: file
-    //     // },
-    //     responseType: 'arraybuffer'
-    // }).then(response => {
-    //     console.log('response:', response);
-
-    //     // const blob = response.data;
-    //     // const reader = new FileReader();
-    //     // reader.readAsDataURL(blob);
-    //     // reader.onload=function(e){
-    //     //     const a = document.createElement('a');
-    //     //     a.download = Date.now()+".jpg";
-    //     //     a.href = e.target.result;
-    //     //     document.body.appendChild(a);
-    //     //     a.click();
-    //     //     document.body.removeChild(a);
-    //     // }
-
-    //     // var src = "data:image/jpg;base64,"+btoa(new Uint8Array(response.data).reduce((data, byte)=>data+String.fromCharCode(byte), ''));
-
-    //     var eleLink = document.createElement('a');
-    //     eleLink.download = Date.now() + ".txt";
-    //     eleLink.style.display = 'none';
-    //     var blob = new Blob([response.data],{
-    //         type: 'plain/text'
-    //     });
-    //     // console.log('src:', src);
-    //     eleLink.href = URL.createObjectURL(blob);
-    //     document.body.appendChild(eleLink);
-    //     eleLink.click();
-    //     document.body.removeChild(eleLink);
-    //     window.URL.revokeObjectURL(blob)
-    // }).catch(err => {
-    //     message.error(err.message)
-    // })
-
+    // 20191129
     var xhr = new XMLHttpRequest();
-    // xhr.open('POST', baseURL + '/DownloadFile', true);
-    xhr.open('GET', 'http://e.hiphotos.baidu.com/image/pic/item/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg', true);
+    xhr.open('POST', baseURL + '/DownloadFile', true);
+    // xhr.open('GET', 'http://e.hiphotos.baidu.com/image/pic/item/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg', true);
     xhr.responseType = 'blob';
     xhr.onload = function () {
         if (this.status == 200) {
@@ -69,6 +28,35 @@ async function download(url) {
         message.error(this.statusText);
     }
     xhr.send();
+
+    // axios.post('/DownloadFile', {
+    //     headers: {
+    //         "Access-Control-Allow-Origin": "http://localhost:3000",
+    //         'Content-Type': 'application/json'
+    //     },
+    //     responseType: 'arraybuffer'
+    // }).then(response => {
+    //     console.log('response.data:', response.data);//btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), '')));
+    //     var content = btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));// btoa(response.data.reduce((data, byte) => data + String.fromCharCode(byte), ''));
+
+    //     console.log('content:', content)
+    //     var a = document.createElement('a');
+    //     // var img  = document.createElement('img');
+    //     var blob = new Blob([content]);
+    //     // img.src = content;
+    //     console.log('blob:', blob)
+    //     // var headerName = xhr.getResponseHeader('Content-Disposition');
+    //     var fileName = Date.now() + ".jpg";// decodeURIComponent(headerName).substring(20);
+    //     // console.log('fileName:', fileName);
+    //     a.download = fileName;
+    //     a.href = URL.createObjectURL(blob);
+    //     a.click();
+    //     URL.revokeObjectURL(blob);
+    //     // document.body.appendChild(img);
+    //     message.success('下载成功');
+    // }).catch(err => {
+    //     console.log('axios err:', err.message);
+    // })
 }
 
 export { download }
