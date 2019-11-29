a0961db13f07ef66876db295226a1f8e31e04199
kevinjang <kevinjang@163.com>
2019-11-29 16:22:26 +0800

    Update Login.js
    登录页特效。

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Login } from 'ant-design-pro'
-import './Login.css'
+// import
 const { Tab, UserName, Password, Submit, Mobile, Captcha } = Login
 import { validateUserInfo } from '../../utils/request'
 import { connect } from 'dva'
 import { Alert } from 'antd'
 import router from 'umi/router'
 
+import styles from './Login.css'
+
 @connect(
-    state => ({rules: state.vrules})
+    state => ({ rules: state.vrules })
 )
 class Login1 extends React.Component {
     constructor(props) {
@@ -33,7 +35,7 @@ class Login1 extends React.Component {
 
             console.log('onLoginSubmit-router', router)
             router.push('/mainframe');
-        }).catch(err=>{
+        }).catch(err => {
             router.push('/mainframe');
         });
     }
@@ -50,35 +52,48 @@ class Login1 extends React.Component {
     }
 
     render() {
-        return <div style={{ paddingTop: '25vh', marginLeft: '37.5vw', marginRight: '15vw', maxWidth: '25vw', alignContent: 'middle' }}>
-            <Login
-                defaultActiveKey="tab1"
-                onSubmit={this.onLoginSubmit}
-                onTabChange={this.onTabChange}>
-                <Tab key="tab1" tab="账号登录">
-                    <UserName
-                        name="username"
-                        placeholder="请输入用户名"
-                        rules={this.props.rules[0].userNameRules}
+        console.log(window.location)
+        return <div style={{
+            paddingTop: '25vh',
+            marginLeft: '37.5vw',
+            marginRight: '15vw',
+            // paddingBottom: '37vh',
+            maxWidth: '25vw',
+            alignContent: 'middle'
+        }}>
+            <div className={styles.innerHeader} style={{ borderRadius: '4px' }}>
+                <Login
+                    defaultActiveKey="tab1"
+                    onSubmit={this.onLoginSubmit}
+                    onTabChange={this.onTabChange}>
+                    <Tab key="tab1" tab="账号登录">
+                        <UserName
+                            name="username"
+                            placeholder="请输入用户名"
+                            rules={this.props.rules[0].userNameRules}
 
-                    ></UserName>
-                    <Password name="pw" placeholder="请输入密码" rules={this.props.rules[0].pwRules}></Password>
-                </Tab>
-                <Tab key="tab2" tab="手机验证码登录">
-                    <Mobile name="mobile" placeholder="请输入手机号" rules={this.props.rules[0].mobileRules}></Mobile>
-                    <Captcha name="captcha" placeholder="验证码" buttontext='获取验证码'
-                        onGetCaptcha={() => this.getCaptcha()}>
-                        {/* <buttonText></buttonText>     */}
-                    </Captcha>
-                </Tab>
-                <Submit>登录</Submit>
-            </Login>
-            {this.state.msg ? <Alert
-                message={this.state.msg}
-                showIcon
-                type={this.state.msgType === '0' ? 'success' : 'error'}
-                closable></Alert> : ''}
-        </div>
+                        ></UserName>
+                        <Password name="pw" placeholder="请输入密码" rules={this.props.rules[0].pwRules}></Password>
+                    </Tab>
+                    <Tab key="tab2" tab="手机验证码登录">
+                        <Mobile name="mobile" placeholder="请输入手机号" rules={this.props.rules[0].mobileRules}></Mobile>
+                        <Captcha name="captcha" placeholder="验证码" buttontext='获取验证码'
+                            onGetCaptcha={() => this.getCaptcha()}>
+                            {/* <buttonText></buttonText>     */}
+                        </Captcha>
+                    </Tab>
+                    <Submit>登录</Submit>
+                </Login>
+            </div>
+            
+            {
+                this.state.msg ? <Alert
+                    message={this.state.msg}
+                    showIcon
+                    type={this.state.msgType === '0' ? 'success' : 'error'}
+                    closable></Alert> : ''
+            }
+        </div >
     }
 }
 
