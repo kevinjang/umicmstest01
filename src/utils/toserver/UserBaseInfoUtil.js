921e67ffb1cc1a171d8297ec263b46b5d141bfa0
kevinjang <kevinjang@163.com>
2019-12-04 17:20:56 +0800

    Update UserBaseInfoUtil.js
    调用获取架构信息的人员数据。

--- src/utils/toserver/UserBaseInfoUtil.js
+++ src/utils/toserver/UserBaseInfoUtil.js
@@ -11,11 +11,8 @@ async function getUserBaseInfoByAD(userAD, callback) {
         params: { userAD },
         responseType: 'json'
     }).then((response) => {
-        // console.log('get-response-data:', response.data);
         const { data, message: selfMessage } = response.data;
         var row = {}
-        // if (data && Array.isArray(data) && data.length > 0)
-        //     row = data[0][0];
         row = data.recordsets[0][0];
 
         console.log('row:', row);
@@ -39,4 +36,38 @@ async function getUserBaseInfoByAD(userAD, callback) {
     });
 }
 
-export { getUserBaseInfoByAD }
+async function getOULongNameUserBaseInfoByAD(userAD, callback){
+    axios.get(baseURL + '/getOULongNameUserBaseInfoByAD', {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Content-Type': 'application/json'
+        },
+        params: { userAD },
+        responseType: 'json'
+    }).then((response) => {
+        const { data, message: selfMessage } = response.data;
+        var row = {}
+        row = data.recordsets[0][0];
+
+        console.log('row:', row);
+
+        callback({
+            userRow: row,
+            spinning: false
+        })
+        if (selfMessage === 'succeeded') {
+            message.success('人员加载成功')
+        }
+        else {
+            message.error(selfMessage)
+        }
+    }).catch((err) => {
+        console.log({ ...err })
+        callback({
+            spinning: false
+        })
+        message.error(err.message);
+    });
+}
+
+export { getUserBaseInfoByAD, getOULongNameUserBaseInfoByAD }
