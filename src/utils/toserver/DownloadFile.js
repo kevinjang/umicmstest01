a2baf7e3a863e8a995cd9df58b76870b42ef639b
kevinjang <kevinjang@163.com>
2019-12-16 12:04:19 +0800

    Update DownloadFile.js
    指定文件下载工具函数。

--- src/utils/toserver/DownloadFile.js
+++ src/utils/toserver/DownloadFile.js
@@ -59,4 +59,28 @@ async function download(url) {
     // })
 }
 
-export { download }
+async function downloadAppointedFile(fileID) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', baseURL + `/DownloadFile`, true);
+    xhr.responseType = 'blob';
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        console.log('xhr-onload-this.response:', this.response);
+        if (this.status == 200) {
+            var content = this.response;
+            var a = document.createElement('a');
+            var fileName = Date.now() + '.jpg';
+            a.download = fileName;
+            a.href = URL.createObjectURL(content);
+            a.click();
+            URL.revokeObjectURL(content);
+            message.success('下载成功');
+        }
+    }
+    xhr.onerror = () => {
+        message.error(this.statusText)
+    }
+    xhr.send(`fileID=${fileID}`);
+}
+
+export { download, downloadAppointedFile }
