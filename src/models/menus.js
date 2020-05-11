145144ba7727100d539a036f08488fa229a5dc55
kevinjang <kevinjang@163.com>
2020-05-11 17:25:31 +0800

    Update menus.js
    新加拖动测试菜单。

--- src/models/menus.js
+++ src/models/menus.js
@@ -4,17 +4,20 @@ export default {
     state: [
         {
             id: '1',
+            level: 1,
             title: 'Option 1',
             icon: 'pie-chart',
             children: [
                 {
                     id: '4',
+                    level:2,
                     title: 'Table数据展示',
                     icon: 'pie-chart',
                     urlPath: 'dashboard',
                     nodeInfo: './dashboard/dashboard'
                 }, {
                     id: '5',
+                    level:2,
                     title: 'Option 5',
                     icon: 'pie-chart',
                     urlPath: 'treetest',
@@ -24,17 +27,20 @@ export default {
         },
         {
             id: '2',
+            level: 1,
             title: 'Option 2',
             icon: 'weibo',
             children: [
                 {
                     id: '6',
+                    level:2,
                     title: '弹窗编辑示例',
                     icon: 'weibo-square',
                     urlPath: 'aladin',
                     nodeInfo: './treetest/Aladin'
                 }, {
                     id: '7',
+                    level:2,
                     title: '网格内编辑示例',
                     icon: 'pie-chart',
                     urlPath: 'formtest',
@@ -44,11 +50,13 @@ export default {
         },
         {
             id: '3',
+            level: 1,
             title: '数据管理中心',
             icon: 'weibo',
-            children:[
+            children: [
                 {
                     id: '17',
+                    level:2,
                     title: '离职查询授权',
                     icon: 'weibo',
                     urlPath: 'LeaveAuthorization',
@@ -56,6 +64,7 @@ export default {
                 },
                 {
                     id: '22',
+                    level:2,
                     title: '员工BP号维护',
                     icon: 'windows',
                     urlPath: 'EmployeeBPMaintain',
@@ -63,6 +72,7 @@ export default {
                 },
                 {
                     id: '27',
+                    level:2,
                     title: '员工年假标准维护',
                     icon: 'twitter',
                     urlPath: 'VacationStandardMaintain',
@@ -72,11 +82,13 @@ export default {
         },
         {
             id: '8',
+            level: 1,
             title: 'Form表单学习',
             icon: 'form',
             children: [
                 {
                     id: '9',
+                    level:2,
                     title: '水平排列登陆表单',
                     icon: 'form',
                     urlPath: 'HorizontalLoginForm',
@@ -84,13 +96,16 @@ export default {
                 },
                 {
                     id: '10',
+                    level:2,
                     title: '正常登录表单',
                     icon: 'form',
+                    level:2,
                     urlPath: 'NormalLoginForm',
                     nodeInfo: './treetest/NormalLoginForm'
                 },
                 {
                     id: '11',
+                    level:2,
                     title: '注册新用户',
                     icon: 'form',
                     urlPath: 'NewUserRegistration',
@@ -98,6 +113,7 @@ export default {
                 },
                 {
                     id: '12',
+                    level:2,
                     title: '高级搜索',
                     icon: 'form',
                     urlPath: 'MultiConditionSearch',
@@ -105,6 +121,7 @@ export default {
                 },
                 {
                     id: '13',
+                    level:2,
                     title: '弹出层中的新建表单',
                     icon: 'form',
                     urlPath: 'CollectionCreateForm',
@@ -112,6 +129,7 @@ export default {
                 },
                 {
                     id: '14',
+                    level:2,
                     title: '动态增减表单项',
                     icon: 'form',
                     urlPath: 'DynamicFieldSet',
@@ -119,6 +137,7 @@ export default {
                 },
                 {
                     id: '15',
+                    level:2,
                     title: '时间类控件',
                     icon: 'form',
                     urlPath: 'TimeRelatedForm',
@@ -126,6 +145,7 @@ export default {
                 },
                 {
                     id: '16',
+                    level:2,
                     title: '自定义表单控件',
                     icon: 'form',
                     urlPath: 'PriceInput',
@@ -135,11 +155,13 @@ export default {
         },
         {
             id: '18',
+            level: 1,
             title: '官方模块测试',
             icon: 'windows',
             children: [
                 {
                     id: '19',
+                    level:2,
                     title: 'Spin测试',
                     icon: 'windows',
                     urlPath: 'spnningTest',
@@ -147,6 +169,7 @@ export default {
                 },
                 {
                     id: '20',
+                    level:2,
                     title: 'TreeSelect',
                     icon: 'windows',
                     urlPath: 'treeSelectTest',
@@ -154,20 +177,23 @@ export default {
                 },
                 {
                     id: '21',
+                    level:2,
                     title: '异步加载树形',
                     icon: 'windows',
                     urlPath: 'treeAsyncTest',
                     nodeInfo: './compoTests/treeAsyncTest'
-                },                
+                },
                 {
                     id: '28',
+                    level:2,
                     title: '照片墙',
                     icon: 'windows',
                     urlPath: 'UploadMain',
                     nodeInfo: './UploadTest/UploadMain'
-                },             
+                },
                 {
                     id: '29',
+                    level:2,
                     title: '拖拽上传',
                     icon: 'windows',
                     urlPath: 'DragDropUpload',
@@ -177,11 +203,13 @@ export default {
         },
         {
             id: '23',
+            level: 1,
             title: '嵌套路由测试',
             icon: 'html5',
-            children:[
+            children: [
                 {
                     id: '24',
+                    level:2,
                     title: '员工BP号维护',
                     icon: 'windows',
                     urlPath: 'test_layout_index',
@@ -191,16 +219,34 @@ export default {
         },
         {
             id: '25',
+            level: 1,
             title: 'Uncontrolled Components',
             icon: 'html5',
-            children:[
+            children: [
                 {
                     id: '26',
+                    level:2,
                     title: 'FileInput',
                     icon: 'windows',
                     urlPath: 'FileInput',
                     nodeInfo: './UncontrolledComponents/FileInput'
                 },
+            ]
+        },
+        {
+            id: '30',
+            level: 1,
+            title: '拖动实验',
+            icon: 'html5',
+            children: [
+                {
+                    id: '31',
+                    level:2,
+                    title: 'Card Drag N Drop',
+                    icon: 'windows',
+                    urlPath: 'CardDragNDrop',
+                    nodeInfo: './DragNDrop/CardDND'
+                }
             ]
         }
     ]
