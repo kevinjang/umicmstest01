226120548fe9e9e8c4a22e674cd9c7693c777752
kevinjang <kevinjang@163.com>
2020-05-14 14:26:45 +0800

    Update createNewComponent.js
    应该对数组的深浅拷贝有进一步的应用和理解了！！！

--- src/pages/compoTests/createNewComponent.js
+++ src/pages/compoTests/createNewComponent.js
@@ -4,14 +4,9 @@ import { Layout, Button, Input } from 'antd';
 const { Sider, Content } = Layout
 
 function createNewComponent() {
-    // const firstRef = React.createRef();
+    const [components, setComponents] = React.useState([(props) => { return <Input key={"selfmadeInput_" + (components.length - 1)} {...props} /> }])
 
-    const [components, setComponents] = React.useState([(props) => {
-        return <Input key="selfmadeInput_0" {...props} />;
-    }])
-
-    const [tests, setTests] = React.useState(['0'])
-
+    const [tests, setTests ] = React.useState([])
     return (
         <Layout style={{ bottom: '0px', backgroundColor: 'lightcyan', width: '97%', top: '0px', position: 'absolute' }}>
             <Sider width={"100px"} >
@@ -22,21 +17,33 @@ function createNewComponent() {
                     //     setComponents(componentsX)
                     // });
                     const componentsX = components;
-                    componentsX.push((props) => { return <Input key={"selfmadeInput_" + (components.length - 1)} {...props} /> });
-                    setComponents(componentsX)
-                    setTests(['0', '1'])
+                    componentsX.push((props) => { return <Input {...props} /> });
+                    setComponents([...componentsX]);
+                    // console.log('after pushed:', components)
+                    // setTests(['0', '1'])
+
+                    // const ComponentX = (props)=>{
+                    //     return <Input key="componentX_1" type="text" />
+                    // }
+
+                    // const What = React.createElement(ComponentX);
+
+                    // console.log('what:', What);
+
+                    // setComponents([...])
+
+                    // document.querySelector('#content_1').appendChild();
                 }}>生成</Button>
             </Sider>
-            <Content style={{ backgroundColor: 'violet' }}>
+            <Content style={{ backgroundColor: 'violet' }} id="content_1">
                 {
                     components.map((ComponentX, index) => {
-                        // console.log('rendering:', ComponentX);
-                        return <ComponentX />
+                        return <ComponentX key={"selfmade_" + index} />
                     })
                 }
                 {/* {
-                    tests.map((test) => {
-                        return <div>{test}</div>
+                    tests.map((test,index)=>{
+                    return <div key={index}>{test}</div>
                     })
                 } */}
             </Content>
