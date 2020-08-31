12b994ab454ebe2b2275efaa0c60d8c2dcff5edd
kevinjang <kevinjang@163.com>
2020-08-31 12:20:12 +0800

    Create SidebarMenu.jsx
    新增SidebarMenu组件，实现左侧菜单组件化。

---
+++ src/components/SidebarMenus/SidebarMenu.jsx
@@ -0,0 +1,64 @@
+import { Layout, Menu, Icon, notification } from 'antd'
+const { Sider } = Layout;
+const MenuItem = Menu.Item;
+const { SubMenu } = Menu;
+import { Scrollbars } from 'react-custom-scrollbars'
+
+import { Link } from 'umi'
+
+const SidebarMenus = (props) => {
+    const { menus, theme, menuMode, menuCollapsed } = props;
+    return (
+        <Sider {...props}>
+            <Scrollbars>
+                <Menu theme={theme} mode={menuMode} inlineCollapsed={menuCollapsed}>
+                    {menus.map((item, index) => {
+                        if (item.children) {
+                            return (<SubMenu
+                                key={item.id}
+                                title={
+                                    <span><Icon type={item.icon} /><span>{item.title}</span></span>
+                                }>
+                                {
+                                    item.children.map(cItem => {
+                                        return (
+                                            <MenuItem
+                                                key={cItem.id + "_" + cItem.nodeInfo}>
+                                                <Icon type={cItem.icon}></Icon>
+                                                <span>
+                                                    <Link
+                                                        style={{ color: 'white' }}
+                                                        to={cItem.urlPath}
+                                                        key={cItem.id + '_' + cItem.nodeInfo + '_' + cItem.urlPath}
+                                                        onClick={() => notification.info({
+                                                            message: 'test',
+                                                            description: 'test'
+                                                        })}
+                                                    >
+                                                        {cItem.title}    
+                                                    </Link></span>
+                                            </MenuItem>
+                                        )
+                                    })
+                                }
+                            </SubMenu>)
+                        }
+                        else
+                            return <MenuItem key={item.id}>
+                                <Icon type={item.icon}></Icon>
+                                <span>
+                                    <Link style={{color:'white'}} to={cItem.urlPath} key={cItem.id + '_' + cItem.nodeInfo + '_' + cItem.urlPath} 
+                                        onClick={() => notification.info({
+                                            message: 'test',
+                                            description: 'test'
+                                        })}> {cItem.title}</Link>
+                                </span>
+                            </MenuItem>
+                    })}
+                </Menu>
+            </Scrollbars>
+        </Sider>
+    )
+}
+
+export default SidebarMenus;
