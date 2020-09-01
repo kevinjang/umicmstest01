6ec05017ce327ae5c847a0ca12dee6ba40d59b39
kevinjang <kevinjang@163.com>
2020-09-01 17:13:27 +0800

    Update index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,35 @@
 import { Login, } from 'ant-design-pro'
-import { Tabs } from 'antd'
-const { Tab } = Login
+import { Tabs, Form } from 'antd'
+const { Tab, UserName } = Login
 
 import LoginContext from './components/LoginContext'
+const { TabPane } = Tabs
+const TabChildren = []
+
+
+
 
 const Login1 = (props) => {
     return (
-        <LoginContext.Provider>
-            <Form form={props.form}>
-                <Tab tab={"hello"}>
+        <LoginContext.Provider value={{
+            tabUtil: {
+                addTab: (id) => {
 
-                </Tab>
+                },
+                removeTab: (id) => {
+
+                }
+            }
+        }}>
+            <Form form={props.form}>
+                <Tabs>
+                    <LoginContext.Consumer tab="密码登陆">
+                        {value => (<TabPane tab="密码登陆"  //tabUtil={value.tabUtil}
+                        >
+                            <UserName name="userName" placeholder="用户名" />
+                        </TabPane>)}
+                    </LoginContext.Consumer>
+                </Tabs>
             </Form>
         </LoginContext.Provider>
     )
