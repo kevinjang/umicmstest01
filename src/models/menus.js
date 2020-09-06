1b098e496eccfa7810b2210e9753b84be886f40e
kevinjang <kevinjang@163.com>
2020-09-06 23:55:05 +0800

    Update menus.js
    菜单数组下沉一级，增加活跃子菜单和选中项变量，待实现。

--- src/models/menus.js
+++ src/models/menus.js
@@ -1,288 +1,294 @@
-export default {
+const MenusModel = {
     namespace: "menus",
     /* 32了*/
-    state: [
-        {
-            id: "1",
-            level: 1,
-            title: "Option 1",
-            icon: "PieChart",
-            children: [
-                {
-                    id: "4",
-                    level: 2,
-                    title: "Table数据展示",
-                    icon: "pie-chart",
-                    urlPath: "/mainframe/dashboard",
-                    path: '',
-                    nodeInfo: "./dashboard/dashboard"
-                }, {
-                    id: "5",
-                    level: 2,
-                    title: "Option 5",
-                    icon: "pie-chart",
-                    path: '/mainframe/treetest',
-                    urlPath: "treetest",
-                    nodeInfo: "./treetest/treetest"
-                }
-            ]
-        },
-        {
-            id: "2",
-            level: 1,
-            title: "Option 2",
-            icon: "weibo",
-            children: [
-                {
-                    id: "6",
-                    level: 2,
-                    title: "弹窗编辑示例",
-                    icon: "weibo-square",
-                    urlPath: "aladin",
-                    path: 'aladin',
-                    nodeInfo: "./treetest/Aladin"
-                }, {
-                    id: "7",
-                    level: 2,
-                    title: "网格内编辑示例",
-                    icon: "pie-chart",
-                    urlPath: "formtest",
-                    nodeInfo: "./treetest/FormTest"
-                }
-            ]
-        },
-        {
-            id: "3",
-            level: 1,
-            title: "数据管理中心",
-            icon: "weibo",
-            children: [
-                {
-                    id: "17",
-                    level: 2,
-                    title: "离职查询授权",
-                    icon: "weibo",
-                    urlPath: "LeaveAuthorization",
-                    nodeInfo: "./BPM_RM/LeaveAuthorization"
-                },
-                {
-                    id: "22",
-                    level: 2,
-                    title: "员工BP号维护",
-                    icon: "windows",
-                    urlPath: "/mainframe/EmployeeBPMaintain",
-                    nodeInfo: "./BPM_RM/EmployeeBPMaintain"
-                },
-                {
-                    id: "27",
-                    level: 2,
-                    title: "员工年假标准维护",
-                    icon: "twitter",
-                    urlPath: "VacationStandardMaintain",
-                    nodeInfo: "./BPM_RM/VacationStandardMaintain"
-                }
-            ]
-        },
-        {
-            id: "8",
-            level: 1,
-            title: "Form表单学习",
-            icon: "form",
-            children: [
-                {
-                    id: "9",
-                    level: 2,
-                    title: "水平排列登陆表单",
-                    icon: "form",
-                    urlPath: "HorizontalLoginForm",
-                    nodeInfo: "./treetest/HorizontalLoginForm"
-                },
-                {
-                    id: "10",
-                    level: 2,
-                    title: "正常登录表单",
-                    icon: "form",
-                    level: 2,
-                    urlPath: "NormalLoginForm",
-                    nodeInfo: "./treetest/NormalLoginForm"
-                },
-                {
-                    id: "11",
-                    level: 2,
-                    title: "注册新用户",
-                    icon: "form",
-                    urlPath: "NewUserRegistration",
-                    nodeInfo: "./treetest/NewUserRegistration"
-                },
-                {
-                    id: "12",
-                    level: 2,
-                    title: "高级搜索",
-                    icon: "form",
-                    urlPath: "MultiConditionSearch",
-                    nodeInfo: "./treetest/MultiConditionSearch"
-                },
-                {
-                    id: "13",
-                    level: 2,
-                    title: "弹出层中的新建表单",
-                    icon: "form",
-                    urlPath: "CollectionCreateForm",
-                    nodeInfo: "./treetest/CollectionCreateForm"
-                },
-                {
-                    id: "14",
-                    level: 2,
-                    title: "动态增减表单项",
-                    icon: "form",
-                    urlPath: "DynamicFieldSet",
-                    nodeInfo: "./treetest/DynamicFieldSet"
-                },
-                {
-                    id: "15",
-                    level: 2,
-                    title: "时间类控件",
-                    icon: "form",
-                    urlPath: "TimeRelatedForm",
-                    nodeInfo: "./OtherForms/TimeRelatedForm"
-                },
-                {
-                    id: "16",
-                    level: 2,
-                    title: "自定义表单控件",
-                    icon: "form",
-                    urlPath: "PriceInput",
-                    nodeInfo: "./OtherForms/PriceInput"
-                }
-            ]
-        },
-        {
-            id: "18",
-            level: 1,
-            title: "官方模块测试",
-            icon: "windows",
-            children: [
-                {
-                    id: "19",
-                    level: 2,
-                    title: "Spin测试",
-                    icon: "windows",
-                    urlPath: "spnningTest",
-                    nodeInfo: "./compoTests/spnningTest"
-                },
-                {
-                    id: "20",
-                    level: 2,
-                    title: "TreeSelect",
-                    icon: "windows",
-                    urlPath: "treeSelectTest",
-                    nodeInfo: "./compoTests/treeSelectTest"
-                },
-                {
-                    id: "21",
-                    level: 2,
-                    title: "异步加载树形",
-                    icon: "windows",
-                    urlPath: "treeAsyncTest",
-                    nodeInfo: "./compoTests/treeAsyncTest"
-                },
-                {
-                    id: "28",
-                    level: 2,
-                    title: "照片墙",
-                    icon: "windows",
-                    urlPath: "UploadMain",
-                    nodeInfo: "./UploadTest/UploadMain"
-                },
-                {
-                    id: "29",
-                    level: 2,
-                    title: "拖拽上传",
-                    icon: "windows",
-                    urlPath: "DragDropUpload",
-                    nodeInfo: "./UploadTest/DragDropUpload"
-                },
-            ]
-        },
-        {
-            id: "23",
-            level: 1,
-            title: "嵌套路由测试",
-            icon: "html5",
-            children: [
-                {
-                    id: "24",
-                    level: 2,
-                    title: "员工BP号维护",
-                    icon: "windows",
-                    urlPath: "test_layout_index",
-                    nodeInfo: "./test_layout/index"
-                },
-            ]
-        },
-        {
-            id: "25",
-            level: 1,
-            title: "Uncontrolled Components",
-            icon: "html5",
-            children: [
-                {
-                    id: "26",
-                    level: 2,
-                    title: "FileInput",
-                    icon: "windows",
-                    urlPath: "FileInput",
-                    nodeInfo: "./UncontrolledComponents/FileInput"
-                },
-            ]
-        },
-        {
-            id: "30",
-            level: 1,
-            title: "拖动实验",
-            icon: "html5",
-            children: [
-                {
-                    id: "31",
-                    level: 2,
-                    title: "Card Drag N Drop",
-                    icon: "windows",
-                    urlPath: "CardDragNDrop",
-                    nodeInfo: "./DragNDrop/CardDND"
-                },
-                {
-                    id: "32",
-                    level: 2,
-                    title: "CreateNewComponent",
-                    icon: "windows",
-                    urlPath: "CreateNewComponent",
-                    nodeInfo: "./compoTests/createNewComponent"
-                }
-            ]
-        },
-        {
-            id: "33",
-            level: 1,
-            title: "上传测试",
-            icon: "file",
-            children: [
-                {
-                    id: "34",
-                    level: 2,
-                    title: "ImageUploadTest",
-                    icon: "windows",
-                    urlPath: "ImageUploadTest",
-                    nodeInfo: "./FileUploadTest/ImageUploadTest1"
-                },
-                {
-                    id: "35",
-                    level: 2,
-                    title: "FileUploadTest",
-                    icon: "windows",
-                    urlPath: "FileUploadTest",
-                    nodeInfo: "./FileUploadTest/FileUploadTest1"
-                }
-            ]
-        }
-    ]
+    state: {
+        menus:[
+            {
+                id: "1",
+                level: 1,
+                title: "Option 1",
+                icon: "PieChart",
+                children: [
+                    {
+                        id: "4",
+                        level: 2,
+                        title: "Table数据展示",
+                        icon: "PieChart",
+                        urlPath: "/mainframe/dashboard",
+                        // path: '',
+                        nodeInfo: "./dashboard/dashboard"
+                    }, {
+                        id: "5",
+                        level: 2,
+                        title: "Option 5",
+                        icon: "PieChart",
+                        // path: '/mainframe/treetest',
+                        urlPath: "/mainframe/treetest",
+                        nodeInfo: "./treetest/treetest"
+                    }
+                ]
+            },
+            {
+                id: "2",
+                level: 1,
+                title: "Option 2",
+                icon: "Weibo",
+                children: [
+                    {
+                        id: "6",
+                        level: 2,
+                        title: "弹窗编辑示例",
+                        icon: "Weibo",
+                        urlPath: "aladin",
+                        path: 'aladin',
+                        nodeInfo: "./treetest/Aladin"
+                    }, {
+                        id: "7",
+                        level: 2,
+                        title: "网格内编辑示例",
+                        icon: "PieChart",
+                        urlPath: "formtest",
+                        nodeInfo: "./treetest/FormTest"
+                    }
+                ]
+            },
+            {
+                id: "3",
+                level: 1,
+                title: "数据管理中心",
+                icon: "Weibo",
+                children: [
+                    {
+                        id: "17",
+                        level: 2,
+                        title: "离职查询授权",
+                        icon: "Weibo",
+                        urlPath: "LeaveAuthorization",
+                        nodeInfo: "./BPM_RM/LeaveAuthorization"
+                    },
+                    {
+                        id: "22",
+                        level: 2,
+                        title: "员工BP号维护",
+                        icon: "Windows",
+                        urlPath: "/mainframe/EmployeeBPMaintain",
+                        nodeInfo: "./BPM_RM/EmployeeBPMaintain"
+                    },
+                    {
+                        id: "27",
+                        level: 2,
+                        title: "员工年假标准维护",
+                        icon: "Twitter",
+                        urlPath: "VacationStandardMaintain",
+                        nodeInfo: "./BPM_RM/VacationStandardMaintain"
+                    }
+                ]
+            },
+            {
+                id: "8",
+                level: 1,
+                title: "Form表单学习",
+                icon: "Form",
+                children: [
+                    {
+                        id: "9",
+                        level: 2,
+                        title: "水平排列登陆表单",
+                        icon: "Form",
+                        urlPath: "HorizontalLoginForm",
+                        nodeInfo: "./treetest/HorizontalLoginForm"
+                    },
+                    {
+                        id: "10",
+                        level: 2,
+                        title: "正常登录表单",
+                        icon: "Form",
+                        level: 2,
+                        urlPath: "NormalLoginForm",
+                        nodeInfo: "./treetest/NormalLoginForm"
+                    },
+                    {
+                        id: "11",
+                        level: 2,
+                        title: "注册新用户",
+                        icon: "Form",
+                        urlPath: "NewUserRegistration",
+                        nodeInfo: "./treetest/NewUserRegistration"
+                    },
+                    {
+                        id: "12",
+                        level: 2,
+                        title: "高级搜索",
+                        icon: "Form",
+                        urlPath: "MultiConditionSearch",
+                        nodeInfo: "./treetest/MultiConditionSearch"
+                    },
+                    {
+                        id: "13",
+                        level: 2,
+                        title: "弹出层中的新建表单",
+                        icon: "Form",
+                        urlPath: "CollectionCreateForm",
+                        nodeInfo: "./treetest/CollectionCreateForm"
+                    },
+                    {
+                        id: "14",
+                        level: 2,
+                        title: "动态增减表单项",
+                        icon: "Form",
+                        urlPath: "DynamicFieldSet",
+                        nodeInfo: "./treetest/DynamicFieldSet"
+                    },
+                    {
+                        id: "15",
+                        level: 2,
+                        title: "时间类控件",
+                        icon: "Form",
+                        urlPath: "TimeRelatedForm",
+                        nodeInfo: "./OtherForms/TimeRelatedForm"
+                    },
+                    {
+                        id: "16",
+                        level: 2,
+                        title: "自定义表单控件",
+                        icon: "Form",
+                        urlPath: "PriceInput",
+                        nodeInfo: "./OtherForms/PriceInput"
+                    }
+                ]
+            },
+            {
+                id: "18",
+                level: 1,
+                title: "官方模块测试",
+                icon: "Windows",
+                children: [
+                    {
+                        id: "19",
+                        level: 2,
+                        title: "Spin测试",
+                        icon: "Windows",
+                        urlPath: "spnningTest",
+                        nodeInfo: "./compoTests/spnningTest"
+                    },
+                    {
+                        id: "20",
+                        level: 2,
+                        title: "TreeSelect",
+                        icon: "Windows",
+                        urlPath: "treeSelectTest",
+                        nodeInfo: "./compoTests/treeSelectTest"
+                    },
+                    {
+                        id: "21",
+                        level: 2,
+                        title: "异步加载树形",
+                        icon: "Windows",
+                        urlPath: "treeAsyncTest",
+                        nodeInfo: "./compoTests/treeAsyncTest"
+                    },
+                    {
+                        id: "28",
+                        level: 2,
+                        title: "照片墙",
+                        icon: "Windows",
+                        urlPath: "UploadMain",
+                        nodeInfo: "./UploadTest/UploadMain"
+                    },
+                    {
+                        id: "29",
+                        level: 2,
+                        title: "拖拽上传",
+                        icon: "Windows",
+                        urlPath: "DragDropUpload",
+                        nodeInfo: "./UploadTest/DragDropUpload"
+                    },
+                ]
+            },
+            {
+                id: "23",
+                level: 1,
+                title: "嵌套路由测试",
+                icon: "Html5",
+                children: [
+                    {
+                        id: "24",
+                        level: 2,
+                        title: "员工BP号维护",
+                        icon: "Windows",
+                        urlPath: "test_layout_index",
+                        nodeInfo: "./test_layout/index"
+                    },
+                ]
+            },
+            {
+                id: "25",
+                level: 1,
+                title: "Uncontrolled Components",
+                icon: "Html5",
+                children: [
+                    {
+                        id: "26",
+                        level: 2,
+                        title: "FileInput",
+                        icon: "Windows",
+                        urlPath: "FileInput",
+                        nodeInfo: "./UncontrolledComponents/FileInput"
+                    },
+                ]
+            },
+            {
+                id: "30",
+                level: 1,
+                title: "拖动实验",
+                icon: "Html5",
+                children: [
+                    {
+                        id: "31",
+                        level: 2,
+                        title: "Card Drag N Drop",
+                        icon: "Windows",
+                        urlPath: "CardDragNDrop",
+                        nodeInfo: "./DragNDrop/CardDND"
+                    },
+                    {
+                        id: "32",
+                        level: 2,
+                        title: "CreateNewComponent",
+                        icon: "Windows",
+                        urlPath: "CreateNewComponent",
+                        nodeInfo: "./compoTests/createNewComponent"
+                    }
+                ]
+            },
+            {
+                id: "33",
+                level: 1,
+                title: "上传测试",
+                icon: "File",
+                children: [
+                    {
+                        id: "34",
+                        level: 2,
+                        title: "ImageUploadTest",
+                        icon: "Windows",
+                        urlPath: "ImageUploadTest",
+                        nodeInfo: "./FileUploadTest/ImageUploadTest1"
+                    },
+                    {
+                        id: "35",
+                        level: 2,
+                        title: "FileUploadTest",
+                        icon: "Windows",
+                        urlPath: "FileUploadTest",
+                        nodeInfo: "./FileUploadTest/FileUploadTest1"
+                    }
+                ]
+            }
+        ],
+        activeSubMebu: '',
+        selectedMenuItem: ''
+    }
 }
+
+export default MenusModel
