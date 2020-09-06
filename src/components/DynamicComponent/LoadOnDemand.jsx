ad9efcac04b3419521fb8f763cbc1627a97fa39e
kevinjang <kevinjang@163.com>
2020-09-06 23:54:00 +0800

    Delete LoadOnDemand.jsx

--- src/components/DynamicComponent/LoadOnDemand.jsx
+++
@@ -1,9 +0,0 @@
-import { dynamic } from 'umi'
-
-export default dynamic({
-    loader: async function (name) {
-        const HugeA = await import(`@ant-design/icons/${name}Outlined`);
-
-        return HugeA;
-    }
-})
