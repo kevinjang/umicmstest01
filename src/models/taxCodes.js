832ca4eb94b6660bd2cfa8a2df36c65bd58e27de
kevinjang <kevinjang@163.com>
2020-09-07 17:18:23 +0800

    Create taxCodes.js
    新建

---
+++ src/models/taxCodes.js
@@ -0,0 +1,52 @@
+const TaxCodesModel = {
+    namespace: 'taxCodes',
+    state: {
+        values: [
+            {
+                key: '_',
+                text: '--请选择--'
+            }, {
+                key: 'J0',
+                text: '0%应交税费-进项税'
+            }, {
+                key: 'JC',
+                text: '1.5%应交税费-进项税'
+            }, {
+                key: 'J6',
+                text: '3%应交税费-进项税'
+            }, {
+                key: 'J8',
+                text: '4%应交税费-进项税'
+            }, {
+                key: 'J7',
+                text: '5%应交税费-进项税'
+            }, {
+                key: 'J5',
+                text: '6%应交税费-进项税'
+            }, {
+                key: 'J9',
+                text: '7%应交税费-进项税'
+            }, {
+                key: 'JK',
+                text: '9%应交税费-进项税'
+            }, {
+                key: 'JG',
+                text: '10%应交税费-进项税'
+            }, {
+                key: 'J4',
+                text: '11%应交税费-进项税'
+            }, {
+                key: 'J2',
+                text: '13%应交税费-进项税'
+            }, {
+                key: 'JF',
+                text: '16%应交税费-进项税'
+            }, {
+                key: 'JJ',
+                text: '19%应交税费-进项税'
+            }
+        ]
+    }
+}
+
+export default TaxCodesModel
