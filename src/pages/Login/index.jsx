8d4e7510e657c83b8613320b1139cdd42cffc589
kevinjang <kevinjang@163.com>
2020-09-08 17:25:05 +0800

    Update index.jsx
    fill the key

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,8 @@
 
-import { Tabs, Form, Input, Button, Space } from 'antd'
+import { Tabs, Form, Input, Button, Space, notification } from 'antd'
 import LoginForm from './components'
 import { UserOutlined, EyeInvisibleOutlined, LockOutlined } from '@ant-design/icons'
-
+import { checkBackEndRunning } from '../../utils/request'
 import styles from './index.less'
 const { Tab } = LoginForm
 
@@ -29,16 +29,16 @@ const Login1 = (props) => {
                         required: true,
                         message: '请填写用户名！'
                     }
-                ]}><Input placeholder="用户名" key="userName" size="large" prefix={<UserOutlined />} /></Form.Item>
+                ]} key={"fi_un"}><Input placeholder="用户名" key="userName" size="large" prefix={<UserOutlined />} /></Form.Item>
                 <Form.Item name="password" rules={[
                     {
                         required: true,
                         message: '请输入密码！'
                     }
-                ]}>
+                ]} key={"fi_pw"}>
                     <Input.Password placeholder="密码" size="large" key="pwd" prefix={<LockOutlined type="required" />} />
                 </Form.Item>
-                <Form.Item>
+                <Form.Item key={"fi_submit"}>
                     <Button
                         className={styles.submitBtn}
                         size="large"
@@ -46,7 +46,25 @@ const Login1 = (props) => {
                         htmlType="submit"
                         type="primary"
                         onClick={() => {
-                            history.push('/mainframe')
+                            const promiseRet = checkBackEndRunning();
+                            console.log('process:', process)
+                            promiseRet.then(data => {
+                                const running = data.data.running;
+                                if (!!running) {
+                                    history.push('/mainframe')
+                                }
+                                else {
+                                    notification.error({
+                                        message: '服务器端异常，请稍后重试！',
+                                        description: '服务器端异常'
+                                    })
+                                }
+                            }).catch(err => {
+                                notification.error({
+                                    message: '服务器端异常，请稍后重试！',
+                                    description: '服务器端异常'
+                                })
+                            });
                         }}
                         onSubmit={() => {
                             history.push('/mainframe')
