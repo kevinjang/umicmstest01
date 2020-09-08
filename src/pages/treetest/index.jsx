d5a587c6eeb747a70fc8706b9f707f3b9f4c0d94
kevinjang <kevinjang@163.com>
2020-09-08 17:26:08 +0800

    ai

--- src/pages/treetest/index.jsx
+++ src/pages/treetest/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table, Card, Button, Popconfirm, DatePicker, Form, Input, Menu, Dropdown, Icon, InputNumber } from 'antd';
+import { Table, Card, Button, Popconfirm, DatePicker, Form, Input, Menu, Dropdown, Icon, InputNumber, notification } from 'antd';
 import 'antd/dist/antd.css'
 import './treetest.css'
 
@@ -19,255 +19,11 @@ const EditableRow = ({ form, index, ...props }) => {
     return (
         <EditableContext.Consumer>
             {value => {
-                return <tr {...props}>{children}</tr>
+                return <tr>{children}</tr>
             }}
         </EditableContext.Consumer>
     );
 };
-
-// const EditableFormRow = Form.create()(EditableRow);
-
-class EditableCell extends React.Component {
-    constructor(props) {
-        super(props)
-        this.CabinTypeCodes = [{
-            key: '0',
-            text: '--请选择--'
-        },
-        {
-            key: '1',
-            text: '无'
-        },
-        {
-            key: '2',
-            text: '火车，软卧'
-        },
-        {
-            key: '3',
-            text: '火车，硬卧'
-        },
-        {
-            key: '4',
-            text: '火车，硬座'
-        },
-        {
-            key: '5',
-            text: '火车，二等座'
-        },
-        {
-            key: '6',
-            text: '火车，一等座'
-        },
-        {
-            key: '7',
-            text: '火车，商务座'
-        },
-        {
-            key: '8',
-            text: '轮船，二等座'
-        },
-        {
-            key: '9',
-            text: '轮船，一等座'
-        },
-        {
-            key: '10',
-            text: '飞机，经济舱'
-        },
-        {
-            key: '11',
-            text: '飞机，商务舱'
-        },
-        {
-            key: '12',
-            text: '飞机，公务舱'
-        },
-        {
-            key: '13',
-            text: '飞机，头等舱'
-        }
-        ]
-
-        this.TaxCodes = [
-            {
-                key: '_',
-                text: '--请选择--'
-            }, {
-                key: 'J0',
-                text: '0%应交税费-进项税'
-            }, {
-                key: 'JC',
-                text: '1.5%应交税费-进项税'
-            }, {
-                key: 'J6',
-                text: '3%应交税费-进项税'
-            }, {
-                key: 'J8',
-                text: '4%应交税费-进项税'
-            }, {
-                key: 'J7',
-                text: '5%应交税费-进项税'
-            }, {
-                key: 'J5',
-                text: '6%应交税费-进项税'
-            }, {
-                key: 'J9',
-                text: '7%应交税费-进项税'
-            }, {
-                key: 'JK',
-                text: '9%应交税费-进项税'
-            }, {
-                key: 'JG',
-                text: '10%应交税费-进项税'
-            }, {
-                key: 'J4',
-                text: '11%应交税费-进项税'
-            }, {
-                key: 'J2',
-                text: '13%应交税费-进项税'
-            }, {
-                key: 'JF',
-                text: '16%应交税费-进项税'
-            }, {
-                key: 'JJ',
-                text: '19%应交税费-进项税'
-            }];
-        this.drpControls = ['CabinType', 'ExpenseHotelTaxCode']
-        this.numberControls = ['ExpenseTraffic', 'ExpenseBoat', 'ExpenseBaggage', 'ExpenseHotel', 'ExpenseMeal', 'ExpenseOther', 'ExpenseSum', 'Remark2']
-        this.ctMenu = (<Menu onClick={(e, v) => {
-            const { record } = this.props;
-            record.CabinType = e.key
-            const { handleSave } = this.props;
-            this.toggleEdit();
-            handleSave(record)
-        }}>{this.CabinTypeCodes.map((item, index) => {
-            return <Menu.Item key={item.key} > {item.text}
-            </Menu.Item>
-        })}
-        </Menu>)
-        this.form = null;
-        this.tcMenu = (<Menu onClick={(e, v) => {
-            const { record } = this.props;
-            record.ExpenseHotelTaxCode = e.key
-
-            const { handleSave } = this.props;
-            this.toggleEdit();
-            handleSave(record)
-        }}>{this.TaxCodes.map((item, index) => {
-            return <Menu.Item key={item.key} > {item.text}
-            </Menu.Item>
-        })}</Menu>)
-    }
-
-    state = {
-        editing: false
-    }
-
-    toggleEdit = () => {
-        const editing = !this.state.editing;
-        this.setState({
-            editing,
-        }, () => {
-            if (editing) {
-                if (this.input)
-                    this.input.focus()
-            }
-        })
-    }
-
-    save = (e) => {
-        const { record, handleSave } = this.props;
-        this.form.validateFields((error, values) => {
-            if (error && error[e.currentTarget.id]) {
-                return;
-            }
-            this.toggleEdit();
-            handleSave({ ...record, ...values });
-        })
-    }
-
-    setControl = (dataIndex, that) => {
-        if (this.drpControls.findIndex(item => item === dataIndex) > -1) {
-            const { record, index } = that.props;
-            return <Dropdown overlay={(dataIndex === 'CabinType') ? this.ctMenu : this.tcMenu}>
-                {(dataIndex === 'CabinType') ?
-                    <Button id={"record_drpBtn_" + index} >
-                        {
-                            this.CabinTypeCodes.findIndex(p => p.key === record.CabinType).text
-                        }
-                        {/* <Icon type='down'> </Icon> */}
-                        <DownOutlined />
-                    </Button> :
-                    <Button id={"record_drpTaxCodeBtn_" + index} >
-                        {
-                            this.TaxCodes.find(p => p.key === record.ExpenseHotelTaxCode).text
-                        }
-                        {/* <Icon type='down'> </Icon> */}
-                        <DownOutlined />
-                    </Button>
-                } </Dropdown>
-        } else if (this.numberControls.findIndex(item => item === dataIndex) > -1) {
-            return <InputNumber autoFocus={true} onBlur={that.save} >
-            </InputNumber>
-        } else if (dataIndex === 'ExpenseTime') {
-            let currDate = new Date();
-            return <DatePicker
-                format={dateFormat}
-                onChange={that.save} >
-            </DatePicker>
-        } else {
-            return <Input
-                ref={node => (that.input = node)}
-                onPressEnter={that.save}
-                onBlur={that.save} />
-        }
-    }
-
-    renderCell = form => {
-        this.form = form;
-        const { children, dataIndex, record, title } = this.props;
-        const { editing } = this.state;
-        return editing ? (
-            <Form.Item style={{ margin: 0 }} name={dataIndex} rules={[
-                {
-                    required: true,
-                    message: `${title} is required`
-                }
-            ]} initialValue={record[dataIndex]}>
-                <Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />
-            </Form.Item>
-        ) :
-            (<div className='editable-cell-value-wrap' style={{ paddingRight: 24 }} onClick={this.toggleEdit}>
-                {children}
-            </div>);
-    }
-
-    render() {
-        const {
-            editable,
-            dataIndex,
-            title,
-            record,
-            index,
-            handleSave,
-            children,
-            ...restProps
-        } = this.props;
-
-        return (
-            <td {...restProps}>
-                {editable ?
-                    (<EditableContext.Consumer>
-                        {value => this.renderCell(value.form)}
-                    </EditableContext.Consumer>)
-                    : (children)
-                }
-            </td>
-        );
-    }
-}
-
-
 
 class TreeTest extends React.Component {
     constructor(props) {
@@ -281,136 +37,190 @@ class TreeTest extends React.Component {
             username: 'hello u',
             dataSource: [],
             count: 0,
-            columns: [{
-                key: 'RowNum',
-                title: '序号',
-                dataIndex: 'RowNum',
-                width: 50
-            },
-            {
-                key: 'ExpenseTime',
-                title: '日期',
-                editable: true,
-                dataIndex: 'ExpenseTime',
-                // width: 160,
-                width: 160,
-                align: 'center',
-                render: (text, record, index) => {
-                    return <div> {text && text.format('YYYY/MM/DD')} </div>;
-                }
-            },
-            {
-                key: 'ExpenseAddress',
-                title: '费用发生地',
-                dataIndex: 'ExpenseAddress',
-                align: 'center',
-                width: 100,
-                editable: true,
-            },
-            {
-                key: 'CabinType',
-                title: '舱位',
-                align: 'center',
-                dataIndex: 'CabinType',
-                editable: true,
-                // width: 100,
-                render: (text, record, index) => {
-                    return <div > {this.CabinTypeCodes[this.CabinTypeCodes.findIndex(p => p.key === record.CabinType)].text} </div>
-                }
-            },
-            {
-                key: 'ExpenseTraffic',
-                title: '航空/铁路',
-                align: 'center',
-                dataIndex: 'ExpenseTraffic',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseBoat',
-                title: '公路/水路',
-                align: 'center',
-                dataIndex: 'ExpenseBoat',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseBaggage',
-                title: '出租车/网约车/市内公交',
-                align: 'center',
-                dataIndex: 'ExpenseBaggage',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseHotel',
-                title: '住宿',
-                align: 'center',
-                dataIndex: 'ExpenseHotel',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseHotelTaxCode',
-                title: '税率',
-                align: 'center',
-                dataIndex: 'ExpenseHotelTaxCode',
-                editable: true,
-                // width: 100,
-                render: (text, record, index) => {
-                    return <div > {this.TaxCodes[this.TaxCodes.findIndex(p => p.key === record.ExpenseHotelTaxCode)].text} </div>
-                }
-            },
-            {
-                key: 'ExpenseMeal',
-                title: '餐费',
-                align: 'center',
-                dataIndex: 'ExpenseMeal',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseOther',
-                title: '其他',
-                align: 'center',
-                dataIndex: 'ExpenseOther',
-                editable: true,
-                // width: 80
-            },
-            {
-                key: 'ExpenseSum',
-                title: '费用金额合计',
-                dataIndex: 'ExpenseSum',
-                align: 'center',
-                editable: false,
-                // width: 80,
-                render: (text, record, index) => {
-                    return <div > {
-                        this.getNumberForInput(record.ExpenseTraffic) +
-                        this.getNumberForInput(record.ExpenseBoat) +
-                        this.getNumberForInput(record.ExpenseBaggage) +
-                        this.getNumberForInput(record.ExpenseHotel) +
-                        this.getNumberForInput(record.ExpenseMeal) +
-                        this.getNumberForInput(record.ExpenseOther)
-                    } </div>
-                }
-            },
-            {
-                key: 'InvoiceNo',
-                title: '电子发票号',
-                align: 'center',
-                dataIndex: 'InvoiceNo',
-                editable: true,
-                // width: 100,
-                max: 10
-            },
-            {
-                key: '13',
-                text: '飞机，头等舱'
-            }
-            ],
             formX: null
         }
+        this.columns = [{
+            key: 'RowNum',
+            title: '序号',
+            dataIndex: 'RowNum',
+            width: 50
+        },
+        {
+            key: 'ExpenseTime',
+            title: '日期',
+            editable: true,
+            dataIndex: 'ExpenseTime',
+            // width: 160,
+            required: true,
+            width: 160,
+            align: 'center',
+            render: (text, record, index) => {
+                return <div> {text && text.format('YYYY/MM/DD')} </div>;
+            }
+        },
+        {
+            key: 'ExpenseAddress',
+            title: '费用发生地',
+            dataIndex: 'ExpenseAddress',
+            required: true,
+            align: 'center',
+            width: 100,
+            editable: true,
+        },
+        {
+            key: 'CabinType',
+            title: '舱位',
+            align: 'center',
+            dataIndex: 'CabinType',
+            required: true,
+            editable: true,
+            // width: 100,
+            render: (text, record, index) => {
+                return <div > {this.CabinTypeCodes[this.CabinTypeCodes.findIndex(p => p.key === record.CabinType)].text} </div>
+            }
+        },
+        {
+            key: 'ExpenseTraffic',
+            title: '航空/铁路',
+            align: 'center',
+            dataIndex: 'ExpenseTraffic',
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            },
+            editable: true,
+            // width: 80
+        },
+        {
+            key: 'ExpenseBoat',
+            title: '公路/水路',
+            align: 'center',
+            dataIndex: 'ExpenseBoat',
+            editable: true,
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            }
+            // width: 80
+        },
+        {
+            key: 'ExpenseBaggage',
+            title: '出租车/网约车/市内公交',
+            align: 'center',
+            dataIndex: 'ExpenseBaggage',
+            editable: true,
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            }
+            // width: 80
+        },
+        {
+            key: 'ExpenseHotel',
+            title: '住宿',
+            align: 'center',
+            dataIndex: 'ExpenseHotel',
+            editable: true,
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            }
+            // width: 80
+        },
+        {
+            key: 'ExpenseHotelTaxCode',
+            title: '税率',
+            align: 'center',
+            dataIndex: 'ExpenseHotelTaxCode',
+            editable: true,
+            required: true,
+            // width: 100,
+            render: (text, record, index) => {
+                return <div > {this.TaxCodes[this.TaxCodes.findIndex(p => p.key === record.ExpenseHotelTaxCode)].text} </div>
+            }
+        },
+        {
+            key: 'ExpenseMeal',
+            title: '餐费',
+            align: 'center',
+            dataIndex: 'ExpenseMeal',
+            editable: true,
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            }
+            // width: 80
+        },
+        {
+            key: 'ExpenseOther',
+            title: '其他',
+            align: 'center',
+            dataIndex: 'ExpenseOther',
+            editable: true,
+            required: true,
+            render: (text, record, index) => {
+                return <div>
+                    {text || 0}
+                </div>
+            }
+            // width: 80
+        },
+        {
+            key: 'ExpenseSum',
+            title: '费用金额合计',
+            dataIndex: 'ExpenseSum',
+            align: 'center',
+            editable: false,
+            // width: 80,
+            render: (text, record, index) => {
+                return <div > {
+                    this.getNumberForInput(record.ExpenseTraffic) +
+                    this.getNumberForInput(record.ExpenseBoat) +
+                    this.getNumberForInput(record.ExpenseBaggage) +
+                    this.getNumberForInput(record.ExpenseHotel) +
+                    this.getNumberForInput(record.ExpenseMeal) +
+                    this.getNumberForInput(record.ExpenseOther)
+                } </div>
+            }
+        },
+        {
+            key: 'InvoiceNo',
+            title: '电子发票号',
+            align: 'center',
+            dataIndex: 'InvoiceNo',
+            editable: true,
+            // width: 100,
+            max: 10
+        }, {
+            key: "operations",
+            title: "操作",
+            align: 'center',
+            dataIndex: 'operations',
+            editable: false,
+            render: () => {
+                return (
+                    <div>
+                        <Button size="small" type="dashed" onClick={() => notification.info({
+                            message: 'delete pressed',
+                            description: 'delete?'
+                        })}>
+                            删除
+                        </Button>
+                    </div>
+                )
+            }
+        }
+        ];
 
         this.formRef = React.createRef();
     }
@@ -496,7 +306,7 @@ class TreeTest extends React.Component {
                 // </TreeTestContext.Provider>
             }
         }
-        const columns = this.state.columns.map((col) => {
+        const columns = this.columns.map((col) => {
             if (!col.editable) {
                 return col;
             }
