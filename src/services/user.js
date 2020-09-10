6e87109736aaf3756060d638e7ea7ca7834e164d
kevinjang <kevinjang@163.com>
2020-09-10 17:29:22 +0800

    Update user.js
    扩展请求。

--- src/services/user.js
+++ src/services/user.js
@@ -3,3 +3,7 @@ import request from '@/utils/realRequest'
 export async function queryNotices(){
     return request('/api/notices');
 }
+
+export async function queryERRemoData(){
+    return request('/api/erdata');
+}
