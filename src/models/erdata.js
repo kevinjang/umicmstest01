779a574cf3fdcdae0cc4dae38e9c7fe4c333526e
kevinjang <kevinjang@163.com>
2020-09-14 17:29:11 +0800

    Update erdata.js

--- src/models/erdata.js
+++ src/models/erdata.js
@@ -1,25 +1,38 @@
 import { queryERRemoData } from '../services/user'
+import { find } from 'lodash'
 
 const erDataModel = {
     namespace: 'er_data',
     state: {
-        remoteDataSource: []
+        remoteDataSource: [],
+        editingRecord: null
     },
     effects: {
-        *fetchRemoteData(_, { call,put, select }) {
+        *fetchRemoteData(_, { call, put, select }) {
             const data = yield call(queryERRemoData);
             yield put({
-                type:'saveDataSource',
+                type: 'saveDataSource',
                 payload: data
             })
         }
     },
-    reducers:{
-        'saveDataSource':(state,{payload})=>{
+    reducers: {
+        'saveDataSource': (state, { payload }) => {
             return {
                 ...state,
                 remoteDataSource: payload,
             }
+        },
+        'setEditingRecord': (state, { payload }) => {
+            const { id } = payload;
+            const item = find(state.remoteDataSource, (it) => {
+                return it.key === id;
+            });
+            
+            return {
+                ...state,
+                editingRecord: { ...item }
+            }            
         }
     }
 }
