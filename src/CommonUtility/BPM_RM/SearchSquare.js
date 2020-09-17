71ecb54a7d8db50d15291d719aacbef985247a00
kevinjang <kevinjang@163.com>
2020-09-17 17:28:25 +0800

    Update SearchSquare.js
    删除多余注释

--- src/CommonUtility/BPM_RM/SearchSquare.js
+++ src/CommonUtility/BPM_RM/SearchSquare.js
@@ -1,4 +1,4 @@
-import { Layout, Form, Button, Select, Input, Icon } from 'antd'
+import { Layout, Form, Button, Select, Input, Icon, Space } from 'antd'
 
 const { Header } = Layout;
 
@@ -12,26 +12,18 @@ export default function (props) {
     // const { getFieldDecorator } = props.form;
 
     // const {FormItems} = props.items;
-    
+
     return (
         <div>
             <Header style={{
-                backgroundColor: 'whitesmoke'
-                // , padding: '-10px 0'
-                // , border: '1px solid black'
-                ,height: '40px'
+                backgroundColor: 'whitesmoke',
+                height: '40px'
             }}>
                 <Form>
                     {props.items ?
                         props.items.map((item) => {
-                            console.log('item.obj type:', typeof item.obj)
-                            
-                            const ObjCompo=item.obj;
-                            return <Form.Item style={{ float: 'right', marginBottom: '0px' }} name={item.name}>
-                                {/* {
-                                    getFieldDecorator(item.name)(item.obj)
-                                } */}
-                                {/* <ObjCompo /> */}
+                            // const ObjCompo = item.obj;
+                            return <Form.Item style={{ float: 'right', marginBottom: '0px', marginRight: '5px' }} name={item.name}>
                                 {item.obj}
                             </Form.Item>
                         }) : null
