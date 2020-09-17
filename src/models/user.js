0894e296036298ae21a182f9fc6cfbfc4e7a05cc
kevinjang <kevinjang@163.com>
2020-09-17 17:29:47 +0800

    Update user.js
    添加登录状态管理，待完善

--- src/models/user.js
+++ src/models/user.js
@@ -2,5 +2,12 @@ export default {
     namespace: 'login',
     state:{
         loginState: false
+    },
+    reducers:{
+        'login':()=>{
+            return {
+                loginState: true
+            }
+        }
     }
 }
