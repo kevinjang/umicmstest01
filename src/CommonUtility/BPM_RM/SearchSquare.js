27c0d4c45aa90dfcfb5a41f5b099c09a3217594a
kevinjang <kevinjang@163.com>
2020-09-18 17:22:29 +0800

    Update SearchSquare.js
    更新，不再根据实际传入渲染了，全定制

--- src/CommonUtility/BPM_RM/SearchSquare.js
+++ src/CommonUtility/BPM_RM/SearchSquare.js
@@ -1,35 +1,79 @@
-import { Layout, Form, Button, Select, Input, Icon, Space } from 'antd'
+import { Layout, Form, Button, Select, Input, Icon, Space, message } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { connect } from 'umi'
 
 const { Header } = Layout;
 
 
-export default function (props) {
+const SearchSquare = (props) => {
+    const { selectOptions, loadData } = props
+    const formRef = React.createRef();
+    const getConditions = () => {
+        if (formRef && formRef.current) {
+            const form = formRef.current;
+            return {
+                keyWord: form.getFieldValue("key_name"),
+                keyText: form.getFieldValue("key_text")
+            }
+        }
 
-    // function handleAddRecord(){
+        return null;
+    }
 
-    // }
-
-    // const { getFieldDecorator } = props.form;
-
-    // const {FormItems} = props.items;
+    const setCondition = () => {
+        const condition = getConditions();
+        const { dispatch } = props
+        if (condition && dispatch) {
+            dispatch({
+                type: 'LeaveAuthModel/setSearchCondition',
+                payload: condition
+            })
+            if (loadData) {
+                loadData();
+            }
+        }
+    }
 
     return (
         <div>
-            <Header style={{
-                backgroundColor: 'whitesmoke',
-                height: '40px'
-            }}>
-                <Form>
-                    {props.items ?
-                        props.items.map((item) => {
-                            // const ObjCompo = item.obj;
-                            return <Form.Item style={{ float: 'right', marginBottom: '0px', marginRight: '5px' }} name={item.name}>
-                                {item.obj}
-                            </Form.Item>
-                        }) : null
-                    }
-                </Form>
-            </Header>
+            <Form style={{ display: 'flex' }} ref={formRef}>
+                <Space>
+                    <Form.Item name="key_name">
+                        <Select style={{ width: '150px' }} >
+                            {selectOptions}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="key_text">
+                        <Input style={{ width: '120px' }}
+                            onPressEnter={() => {
+                                // NOTE: dispatch更新searchCondition去
+                                setCondition();
+                            }}
+                            onBlur={() => {
+                                // NOTE: dispatch更新searchCondition去
+                                setCondition();
+                            }}
+                            onChange={() => {
+                                // NOTE: dispatch更新searchCondition去
+                                setCondition();
+                            }}
+                        // suffix={<SearchOutlined onClick={
+                        //     () => {
+                        //         // NOTE: dispatch更新searchCondition去
+                        //         setCondition();
+                        //     }
+                        // }
+                        // />}
+                        />
+                    </Form.Item>
+                </Space>
+            </Form>
         </div>
     );
 }
+
+export default connect(({
+    LeaveAuthModel
+}) => ({
+    LeaveAuthModel
+}))(SearchSquare)
