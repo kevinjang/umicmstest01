2ae7a59ee1d3353f05b1136b7c3266b6350730d3
kevinjang <kevinjang@163.com>
2020-09-18 17:22:48 +0800

    Update leaveauth.js
    分页也加进去了，但是感觉最终要倒灶

--- src/models/leaveauth.js
+++ src/models/leaveauth.js
@@ -1,17 +1,55 @@
+import { getByPage } from '../utils/toserver/BasePeopleUtil'
 const LeaveAuth = {
     namespace: 'LeaveAuthModel',
     state: {
         dataSource: [],
-
+        // NOTE: 所有数据行数
+        allCount: 0,
+        // NOTE: 每页行数
+        pageSize: 10,
+        // NOTE: 当前页
+        currentPage: 0,
+        // NOTE: 总页数
+        totalPageCount: 0,
+        // NOTE: 查询条件
+        searchCondition: null,
+        // NOTE: 加载中
+        spinning: false
     },
     effects: {
-        *fetchData(_, { call, put }) {
-
+        *fetchData(_, { call, put, select }) {
+            const d = yield select(state=>({
+                pageSize:state.LeaveAuthModel.pageSize,
+                startPage: state.LeaveAuthModel.currentPage,
+                condition: state.LeaveAuthModel.searchCondition,                
+            }))
+            yield call(()=>{
+                getByPage(d.pageSize, d.startPage, d.condition,(e)=>{
+                    console.log('great e:', e);
+                    put({
+                        type: 'saveData',
+                        payload: e
+                    })
+                })
+            })
         }
     },
     reducers: {
         'saveData': (state, { payload }) => {
-            
+            return {
+                ...state,
+                allCount: payload.allCount,
+                spinning: payload.spinning,
+                dataSource: [...payload.dataSource]
+            }
+        },
+        'setSearchCondition': (state, { payload }) => {
+            return {
+                ...state,
+                searchCondition: {
+                    ...payload
+                }
+            }
         }
     }
 }
