9851cabdfef9fa4be8e263a67bec27eafc23e625
kevinjang <kevinjang@163.com>
2020-09-21 17:24:35 +0800

    Update leaveauth.js
    规范化

--- src/models/leaveauth.js
+++ src/models/leaveauth.js
@@ -1,4 +1,6 @@
 import { getByPage } from '../utils/toserver/BasePeopleUtil'
+import { queryLeaveAuthData } from '../services/leaveauth'
+import { message as messageComp } from 'antd'
 const LeaveAuth = {
     namespace: 'LeaveAuthModel',
     state: {
@@ -17,31 +19,61 @@ const LeaveAuth = {
         spinning: false
     },
     effects: {
-        *fetchData(_, { call, put, select }) {
-            const d = yield select(state=>({
-                pageSize:state.LeaveAuthModel.pageSize,
-                startPage: state.LeaveAuthModel.currentPage,
-                condition: state.LeaveAuthModel.searchCondition,                
-            }))
-            yield call(()=>{
-                getByPage(d.pageSize, d.startPage, d.condition,(e)=>{
-                    console.log('great e:', e);
-                    put({
-                        type: 'saveData',
-                        payload: e
-                    })
-                })
+        *fetchData({ payload }, { call, put, select }) {
+            const { pageSize, startPage, condition, callback } = payload
+
+            const response = yield queryLeaveAuthData({
+                pageSize,
+                startPage,
+                condition
             })
+
+            /**
+             * NOTE: data结构说明
+             * output:{},
+             * recordset:[], 返回的数据集合，实际请求到的数量为准
+             * recordsets: [Array(N), Array(M)], 一次请求执行的操作返回的所有结果
+             * rowsAffected: [N, M] 执行SQL影响的行的数量
+            */
+            const { data, message } = response
+
+            if (message === "succeeded") {
+                yield put({
+                    type: 'saveDataWithRemoteResponse',
+                    payload: { data, callback }
+                })
+            }
+            else {
+                messageComp.error("请求离职授权查询数据失败：" + message);
+            }
         }
     },
     reducers: {
-        'saveData': (state, { payload }) => {
+        'saveDataWithRemoteResponse': (state, { payload }) => {
+            // console.log('pauload:', payload)
+
+            const { data, callback } = payload
+            callback({
+                PaginationTotal: data.recordsets && data.recordsets[1][0].count,
+                allCount: data.recordsets && data.recordsets[1][0].count,
+                pagi_total: 10,
+                spinning: false,
+                dataSource: data.recordset
+            })
             return {
                 ...state,
-                allCount: payload.allCount,
-                spinning: payload.spinning,
-                dataSource: [...payload.dataSource]
+                allCount: data.recordset && data.recordset.length,
+                dataSource: data.recordset
+                // pageSize: 
+                // totalPageCount: 
             }
+
+            // return {
+            //     ...state,
+            //     allCount: payload.allCount,
+            //     spinning: payload.spinning,
+            //     dataSource: [...payload.dataSource]
+            // }
         },
         'setSearchCondition': (state, { payload }) => {
             return {
