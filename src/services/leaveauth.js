887ce997f5c6bdcaf93cec24a2ed065c183eb80d
kevinjang <kevinjang@163.com>
2020-09-21 22:07:24 +0800

    Update leaveauth.js
    必须是get方法请求，因为服务器端指定了get方法的路由处理方法。

--- src/services/leaveauth.js
+++ src/services/leaveauth.js
@@ -1,9 +1,12 @@
 import request from '../utils/realRequest'
 const baseUrl = "http://localhost:3000";
-export async function queryLeaveAuthData( { pageSize, startPage, condition }){
-    return request(baseUrl+ "/getBasePeople",{
-        pageSize, 
-        startPage, 
-        condition
+export async function queryLeaveAuthData({ pageSize, startPage, condition }) {
+    return request(baseUrl + "/getBasePeople", {
+        method: 'GET',
+        params: {
+            pageSize,
+            startPage,
+            condition
+        }
     })
 }
