c4b6f9f5a61e78be07b929fcdda7cd2e644f1b56
kevinjang <kevinjang@163.com>
2020-09-24 17:27:45 +0800

    Update 404.js
    增加返回登录页面按钮

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,10 @@
-export default ()=>{
+import {Button} from 'antd'
+export default ({history})=>{
+    console.log("history:", history);
     return <div>
         404 Page
+        <div>
+            <Button type="link" href="/">回到登录页面</Button>
+        </div>
     </div>
 }
