c155c14b0251ad1da630591c69fb965d85be19a2
kevinjang <kevinjang@163.com>
2020-09-24 22:26:13 +0800

    Update UrlPathGuard.jsx

--- src/pages/user/UrlPathGuard.jsx
+++ src/pages/user/UrlPathGuard.jsx
@@ -1,20 +1,23 @@
 
-import { flatMapDeep } from 'lodash'
-import { Redirect } from 'umi';
-const UrlPathGuard = ({ routes, history, children }) => {
-    const realPaths = flatMapDeep(flatMapDeep(routes, "routes"), "path");
-    console.log('realPaths& history:', realPaths, history)
+import { flatMapDeep, flatMapDepth } from 'lodash'
+import { Redirect, connect } from 'umi';
+const UrlPathGuard = ({ routes, history, children, loginState }) => {
+    const realPaths = [...flatMapDeep(flatMapDeep(routes, "routes"), "path"), ...flatMapDeep(routes, "path")];
+    console.log('realPaths:', realPaths)
     const currentPath = history.location.pathname;
     var validUrlPath = !!currentPath && realPaths.indexOf(currentPath) > -1;
     // if(){
     // }
+    // const {} = 
     return (
         <>
             {
-                validUrlPath? children : <Redirect to="/404" />
+                loginState ? (validUrlPath ? children : <Redirect to="/404" />) : <Redirect to="/" />
             }
         </>
     )
 }
 
-export default UrlPathGuard
+export default connect(({ login }) => ({
+    loginState: login.loginState
+}))(UrlPathGuard)
