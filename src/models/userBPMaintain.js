8a6fafedbe39b168bbff5f13cea603f23eb5d2a8
kevinjang <kevinjang@163.com>
2020-11-16 17:14:44 +0800

    Update userBPMaintain.js
    需要进一步格式化。

--- src/models/userBPMaintain.js
+++ src/models/userBPMaintain.js
@@ -1,98 +1,61 @@
 
 import { getByPage, insert, update, deleteItem, deleteItems } from '../utils/toserver/EmployeeBPUtil'
 import { queryUserBP } from '../services/userbp'
+import { message as messageComp } from 'antd'
 
 const UserBPMaintain = {
     namespace: 'UserBPMaintainModel',
     state: {
-        data: [],
-        // pagination: {
-        //     current: 1,
-        //     pageSize: 10,
-        //     total: 0
-        // },
-        searchCondition: {
-            // id: '',
-            // property: '',
-            // value: ''
-            // bak
-        },
-        total: 0,
-        loading: true
-
+        dataSource: [],
+        allCount: 0,
+        searchCondition: null
     },
     effects: {
-        *fetchData({ pageSize, current }, { call, put, select }) {
+        *fetchData({ payload }, { select, put }) {
+            const { pageSize, current, callback } = payload
             const condition = yield select(state => state.UserBPMaintainModel.searchCondition);
 
-            yield put({
-                type: 'setLoading',
-                payload: {
-                    loading: true
-                }
-            })
-            const result = yield queryUserBP({
+            const response = yield queryUserBP({
                 pageSize,
-                current,
+                startPage: current,
                 condition
             })
-            console.log('query result:', result)
-            const data = result.data.recordsets;
-            yield put({
-                type: 'setData',
-                payload: {
-                    data: data[0],
-                    total: data[1][0].count
-                }
-            })
-
-            // yield put({
-            //     type: 'setLoading',
-            //     payload: {
-            //         loading: false
-            //     }
-            // })
+            const { data, message } = response
+            if (message === 'succeeded') {
+                yield put({
+                    type: 'saveDataWithRemoteResponse',
+                    payload: {
+                        data,
+                        callback
+                        // total: data.length
+                    }
+                })
+                messageComp.success("查询成功");
+            } else {
+                messageComp.error("请求员工BP号查询数据失败：" + message)
+            }
         }
     },
     reducers: {
-        'getCondition': (state, action) => {
+        'saveDataWithRemoteResponse': (state, { payload }) => {
+            const { data, callback } = payload;
+            if (callback) {
+                callback({
+                    allCount: data.recordsets && data.recordsets[1][0].count,
+                    dataSource: data.recordset
+                })
+            }
+
             return {
-                ...state
+                ...state,
+                allCount: data.recordset && data.recordset.length,
+                dataSource: data.recordset
             }
         },
         'setCondition': (state, { payload }) => {
             return {
                 ...state,
-                searchCondition: payload
-            }
-        },
-        'setData': (state, { payload }) => {
-            const { data, total } = payload
-            return {
-                ...state,
-                data,
-                total,
-                loading: false
-            }
-        },
-        'setPagination': (state, { payload }) => {
-            const { current, pageSize, total } = payload
-            console.log('model setPagination pageSize:', pageSize)
-            return {
-                ...state,
-                pagination: {
-                    ...state.pagination,
-                    current,
-                    pageSize,
-                    total
-                }
-            }
-        },
-        'setLoading': (state, { payload }) => {
-            const { loading } = payload;
-            return {
-                ...state,
-                loading
+                searchCondition: { ...payload }
             }
         }
     }
