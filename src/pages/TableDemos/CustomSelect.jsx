0edb2cfeb9b6faa0170b9e3dbce0dc3804dde591
kevinjang <kevinjang@163.com>
2020-11-19 17:24:09 +0800

    官方模块实现
    上手练习

---
+++ src/pages/TableDemos/CustomSelect.jsx
@@ -0,0 +1,79 @@
+import { Table } from 'antd'
+import { useState } from 'react'
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+    },
+];
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
+}
+
+const CustomSelect = () => {
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+    const onSelectChange = selectedRowKeys => {
+        setSelectedRowKeys(selectedRowKeys);
+    }
+
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+        selections: [
+            Table.SELECTION_ALL,
+            Table.SELECTION_INVERT,
+            {
+                key: 'odd',
+                text: '选择奇数行',
+                onSelect: changableRowKeys => {
+                    let newSelectedRowKeys = [];
+                    newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                        if (index % 2 !== 0) {
+                            return false;
+                        }
+                        return true;
+                    })
+
+                    setSelectedRowKeys(newSelectedRowKeys);
+                }
+            },
+            {
+                key: 'even',
+                text: '选择偶数行',
+                onSelect: changableRowKeys => {
+                    let newSelectedRowKeys = [];
+                    newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                        if (index % 2 !== 0) {
+                            return true;
+                        }
+                        return false;
+                    });
+                    setSelectedRowKeys(newSelectedRowKeys);
+                }
+            }
+        ]
+    }
+
+    return (
+        <Table dataSource={data} columns={columns} rowSelection={rowSelection}></Table>
+    )
+}
+
+export default CustomSelect;
---
+++ src/pages/TableDemos/FilterAndSort.jsx
@@ -0,0 +1,110 @@
+import { Table } from 'antd'
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        filters: [
+            {
+                text: 'Joe',
+                value: 'Joe'
+            },
+            {
+                text: 'Jim',
+                value: 'Jim',
+            },
+            {
+                text: 'Submenu',
+                value: 'Submenu',
+                children: [
+                    {
+                        text: 'Green',
+                        value: 'Green',
+                    },
+                    {
+                        text: 'Black',
+                        value: 'Black',
+                    },
+                ]
+            }
+        ],
+        onFilter: (value, record) => record.name.indexOf(value) === 0,
+        sorter: (a, b) => a.name.length - b.name.length,
+        sortDirections: ['descend']
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.age - b.age,
+        filters: [
+            {
+                text: 32,
+                value: 32
+            },
+            {
+                text: 42,
+                value: 42
+            }
+        ],
+        onFilter: (value, record) => record.age === value
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        filters: [
+            {
+                text: 'London',
+                value: 'London',
+            },
+            {
+                text: 'New York',
+                value: 'New York',
+            },
+        ],
+        filterMultiple: false,
+        onFilter: (value, record) => record.address.indexOf(value) === 0,
+        sorter: (a, b) => a.address.length - b.address.length,
+        sortDirections: ['descend', 'ascend'],
+    },
+];
+
+
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+    },
+    {
+        key: '4',
+        name: 'Jim Red',
+        age: 32,
+        address: 'London No. 2 Lake Park',
+    },
+];
+
+const onChange = (pagination, filters, sorter, extra)=>{
+    console.log('params', pagination, filters, sorter, extra);
+}
+
+const FilterAndSort = ()=>{
+    return (
+        <Table columns={columns} dataSource={data} onChange={onChange}></Table>
+    )
+}
+
+export default FilterAndSort
