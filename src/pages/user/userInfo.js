6277a988d72d0c1284a2771de1c7713bcfc6a756
kevinjang <kevinjang@163.com>
2021-01-24 22:48:44 +0800

    Update userInfo.js
    组件化

--- src/pages/user/userInfo.js
+++ src/pages/user/userInfo.js
@@ -6,8 +6,52 @@ import 'antd/lib/avatar/style/index.css'
 import 'antd/lib/badge/style/index.css'
 
 import { UserContext } from '../UserContextMock'
+import SettingDrawer from '../../components/Drawer'
+import UserMenu from './UserMenu'
+import { NavLink } from 'umi'
+const Menus = (props) => {
+    const {theme} = props
+    return (
+        <Menu style={{ width: '150px' }} >
+            <Menu.Item>
+                <Space><SettingDrawer /></Space>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item>
+                {/* <a><Icon type="lock"></Icon>注销</a> */}
+                <Space><UserMenu /></Space>
+            </Menu.Item>
+        </Menu>
+    );
+}
 
-class UserInfo extends React.Component {
+const UserInfo = (props) => {
+
+
+    return (<UserContext.Consumer>
+        {value => {
+            return (
+                <Dropdown overlay={<Menus theme={value.theme} ></Menus>} trigger={['click']}>
+                    <div style={{ height: 'fit-content', display: 'flex', textAlign: 'center', color: 'whitesmoke' }}>
+                        <div style={{ fontSize: '16px', marginLeft: '5px' }}>
+                            <Avatar
+                                // style={{ float: 'right' }}
+                                style={{ border: '1px solid white', marginRight: '5px' }}
+                                size='normal'
+                                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"></Avatar>
+                            {value && value.userRow && value.userRow.Username}
+                        </div>
+
+                    </div>
+                </Dropdown>
+            );
+        }}
+    </UserContext.Consumer>);
+}
+
+
+
+class UserInfo1 extends React.Component {
 
     constructor(props) {
         super(props);
@@ -18,12 +62,13 @@ class UserInfo extends React.Component {
                     {/* <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
                         设置
                     </a> */}
-                    <Link to={"http://www.alipay.com"}><Space><SettingFilled></SettingFilled><span>设置</span></Space></Link>
+                    {/* <Link to={"http://www.alipay.com"}><Space><SettingFilled></SettingFilled><span>设置</span></Space></Link> */}
+                    <Space><SettingDrawer /></Space>
                 </Menu.Item>
                 <Menu.Divider />
                 <Menu.Item>
                     {/* <a><Icon type="lock"></Icon>注销</a> */}
-                    <Link to="/"><Space><LockFilled></LockFilled><span>注销</span></Space> </Link>
+                    <Space><UserMenu /></Space>
                 </Menu.Item>
             </Menu>
         )
@@ -37,9 +82,10 @@ class UserInfo extends React.Component {
                             <div style={{ fontSize: '16px', marginLeft: '5px' }}>
                                 <Avatar
                                     // style={{ float: 'right' }}
+                                    style={{ border: '1px solid white', marginRight: '5px' }}
                                     size='normal'
                                     src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"></Avatar>
-                                {value && value.userRow &&value.userRow.Username}
+                                {value && value.userRow && value.userRow.Username}
                             </div>
 
                         </div>
