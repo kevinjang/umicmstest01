e879b34635945306f06b59f02a4abe6353683fd2
kevinjang <kevinjang@163.com>
2021-01-24 22:49:01 +0800

    组件化

--- src/components/LayoutHeader/Header.jsx
+++ src/components/LayoutHeader/Header.jsx
@@ -1,47 +1,54 @@
-import { Layout, Row, Col } from 'antd'
+import { Layout, Row, Col, Button, Menu } from 'antd'
 import styles from './Header.css'
 import { ChromeFilled } from '@ant-design/icons'
 const { Header } = Layout;
 
 import GlobalHeaderDropdown from '../NoticeHeader/GlobalHeader/NoticeIconView'
-import UserInfo from '../../pages/user/UserInfo'
+import UserInfo from '../../pages/User/UserInfo'
+import CustomDrawer from '../Drawer/index'
 import classNames from 'classnames'
 import useSizeRef from 'use-size-ref'
 import { UserContext } from '../../pages/UserContextMock'
+import antStyles from 'antd/dist/antd.css'
+
+import BannerDocker from './BannerDocker'
 
 const GlobalHeader = (props) => {
-    const headerStyle = classNames(styles.header, {
+    const headerStyle = classNames(styles.headerRoot, {
         color: 'white'
     });
+    const colClass = classNames(styles.bannerDock, antStyles["ant-col-8"])
+    console.log('colClass:', colClass)
     const [headerContext, { width, height }] = useSizeRef();
     return (
         <UserContext.Consumer>
             {value => {
                 value.sizeInfo = {
-                    ...value.sizeInfo,                    
+                    ...value.sizeInfo,
                     header: {
                         width,
                         height
                     }
                 }
 
-                console.log("header size info:", {width, height})
+                // console.log("header size info:", {width, height})
+                console.log('theme:', value.theme)
                 return (
-                    <div ref={headerContext}>
-                        <Header className={headerStyle}>
-                            <ChromeFilled />
-                            <span>导航</span>
-                            <div className={styles.userInfoNode}>
-                                <div className={styles.innerContainer}>
-                                    <Row gutter={8} justify="center">
-                                        <Col span={8} className={styles.bannerDock}>
-                                            <GlobalHeaderDropdown />
-                                        </Col>
-                                        <Col span={16} className={styles.bannerDock}>
-                                            <UserInfo />
-                                        </Col>
-                                    </Row>
-                                </div>
+                    <div ref={headerContext} className={styles.bannerContent}>
+                        <Header className={styles.header}>
+                            <span> <ChromeFilled />导航</span>
+                            {/* <Menu mode={"horizontal"} style={{ width: '90%' }} theme={value.theme} direction={"rtl"}>
+                                <Menu.Item style={{ float: 'right',marginRight: '0' }}><CustomDrawer /> </Menu.Item>
+                                <Menu.Item style={{ float: 'right' }}><UserInfo /></Menu.Item>
+                                <Menu.Item style={{ padding: '0 15px', float: 'right' }}><GlobalHeaderDropdown /></Menu.Item>
+                            </Menu> */}
+                            <div style={{ width: '90%' }}>
+                                <BannerDocker style={{ float: 'right' }}>
+                                    <UserInfo />
+                                </BannerDocker>
+                                <BannerDocker mode="horizontal" style={{ float: 'right' }}>
+                                    <GlobalHeaderDropdown />
+                                </BannerDocker>
                             </div>
                         </Header>
                     </div>
