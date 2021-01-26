331cee9751295050d5bfbf6ce1466fcc07cd735c
kevinjang <kevinjang@163.com>
2021-01-26 19:20:03 +0800

    Update BannerDocker.jsx

--- src/components/LayoutHeader/BannerDocker.jsx
+++ src/components/LayoutHeader/BannerDocker.jsx
@@ -1,10 +1,12 @@
 import styles from './BannerDocker.css'
+import {Space} from 'antd'
 
 const BannerDocker = (props) => {
     const {mode} = props
     console.log('mode:', mode)
     return (
         <div {...props} className={styles.root}>
+            {/* <Space>{props.children}</Space> */}
             {props.children}
         </div>
     )
